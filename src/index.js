5f17c25a04bfe5e5b8f56e4fdc2da13dc8df0416
Blaumaus <durexp@protonmail.ch>
2021-03-26 03:07:35 +0200

    Added bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 // import { store } from 'store'
 // import { Provider } from 'react-redux'
 import './index.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
