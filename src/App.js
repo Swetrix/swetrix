5978ba589ee446ee60068a78323c452a76eb9ba8
Blaumaus <durexp@protonmail.ch>
2021-03-27 04:04:53 +0200

    Added Signin

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import routes from 'routes'
 import Header from 'components/Header'
 import MainPage from 'pages/MainPage'
 import SignUp from 'pages/Auth/Signup'
+import SignIn from 'pages/Auth/Signin'
 
 export default () => {
   return (
     <>
       <Header />
       <Switch>
-        <Route path={routes.signin} component={<></>} exact />
+        <Route path={routes.signin} component={SignIn} exact />
         <Route path={routes.signup} component={SignUp} exact />
         <Route path={routes.main} component={MainPage} exact />
         <Redirect to={routes.main} />
