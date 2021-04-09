9d929b8ba2077f3266988f33633140bd01fd89ba
Blaumaus <durexp@protonmail.ch>
2021-04-09 03:31:22 +0300

    fixed typo

--- src/mailer/letter.ts
+++ src/mailer/letter.ts
@@ -5,4 +5,5 @@ export enum LetterTemplate {
   MailAddressChangeConfirmation = 'mailaddress-change_confirmation',
   MailAddressHadChanged = 'mailaddress-change_information',
   PasswordChangeRequest = 'password-change_request',
+  GDPRDataExport = 'gdpr_data_export',
 }
