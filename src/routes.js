17db30f9c0745013df666d8814825cf670bd6be7
Blaumaus <durexp@protonmail.ch>
2021-04-09 03:32:50 +0300

    Added 'project' and 'new_password_form' routes

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ export default Object.freeze({
 	signin: '/login',
 	signup: '/signup',
 	reset_password: '/recovery',
+	new_password_form: '/password-reset/:id',
 	main: '/',
 	dashboard: '/dashboard',
 	user_settings: '/settings',
 	verify: '/verify/:id',
 	change_email: '/change-email/:id',
+	project: '/projects/:id',
 })
