4998a4bf2e506fe4226a416c5004a4e3606b78a3
Blaumaus <durexp@protonmail.ch>
2021-04-09 03:35:46 +0300

    Added new line checks; made cancel button optional

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 import BM from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 
-const Modal = ({ title, text, onSubmit, onCancel, cancelText = 'Close', submitText = 'Save changes' }) => (
+const NewLineText = ({ text }) => {
+  return text.split('\n').map(str => <p key={str}>{str}</p>)
+}
+
+const Modal = ({ title, text, onSubmit = () => {}, onCancel, cancelText = 'Close', submitText = 'Save changes' }) => (
   <BM show={true} onHide={onCancel}>
     <BM.Header closeButton>
       <BM.Title>{title}</BM.Title>
     </BM.Header>
-    <BM.Body>{text}</BM.Body>
+    <BM.Body>
+      <NewLineText text={text} />
+    </BM.Body>
     <BM.Footer>
-      <Button variant="secondary" onClick={onCancel}>
-        {cancelText}
-      </Button>
+      {onCancel && (
+        <Button variant="secondary" onClick={onCancel}>
+          {cancelText}
+        </Button>
+      )}
       <Button variant="primary" onClick={onSubmit}>
         {submitText}
       </Button>
