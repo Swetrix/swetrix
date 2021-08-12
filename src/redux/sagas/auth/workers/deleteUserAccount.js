702813674fd944c9a8f99f8692740ace45264ca7
Blue Mouse <durexp@protonmail.ch>
2021-08-13 00:53:08 +0200

    fixed notification was not showing when account deleted

--- src/redux/sagas/auth/workers/deleteUserAccount.js
+++ src/redux/sagas/auth/workers/deleteUserAccount.js
@@ -1,12 +1,14 @@
 import { call, put } from 'redux-saga/effects'
 
 import { authActions } from 'redux/actions/auth'
+import { alertsActions } from 'redux/actions/alerts'
 import { deleteUser } from 'api'
 
 export default function* deleteUserAccountWorker({ payload: { errorCallback } }) {
   try {
     yield call(deleteUser)
     yield put(authActions.deleteAccountSuccess())
+    yield put(alertsActions.accountDeleted('Your account has been deleted'))
   } catch (e) {
     errorCallback(JSON.parse(e.message))
   } finally {
