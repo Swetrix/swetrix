738a299c3adefe9b9bacc1e0502e931bbfb1a7e0
Blue Mouse <durexp@protonmail.ch>
2021-08-16 20:17:53 +0200

    Integrated Sentry error tracking

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ import { BrowserRouter } from 'react-router-dom'
 import { store } from 'redux/store'
 import { Provider } from 'react-redux'
 import { transitions, positions, Provider as AlertProvider } from 'react-alert'
-import AlertTemplate from 'ui/Alert'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 import 'billboard.js/dist/billboard.min.css'
 import 'prismjs/themes/prism-tomorrow.css'
 
+import AlertTemplate from 'ui/Alert'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import './index.css'
+
+Sentry.init({
+  dsn: 'https://ce538bac7e64484d9a30dcb2eadfe69b@o920340.ingest.sentry.io/5865898',
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+})
 
 const options = {
   position: positions.BOTTOM_RIGHT,
@@ -28,7 +36,9 @@ ReactDOM.render(
     <AlertProvider template={AlertTemplate} {...options}>
       <Provider store={store}>
         <BrowserRouter>
-          <App />
+          <Sentry.ErrorBoundary fallback={<p>The app crashed.. :(<br/>Please, tell us about it at contact@swetrix.com</p>}>  
+            <App />
+          </Sentry.ErrorBoundary>
         </BrowserRouter>
       </Provider>
     </AlertProvider>
