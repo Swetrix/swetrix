88cdb125bf9f67cadd387f296b520cc8332a5559
Blue Mouse <durexp@protonmail.ch>
2021-08-17 23:47:07 +0200

    Using a modern loader icon

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { RefreshIcon } from '@heroicons/react/outline'
+import Spin from './icons/Spin'
 
 const Loader = () => (
   <div className='flex justify-center mt-10'>
-    <RefreshIcon className='animate-spin transform rotate-180 h-20 w-20 text-indigo-700' />
+    <Spin className='h-20 w-20 text-indigo-700' />
     <span className='sr-only'>Loading...</span>
   </div>
 )
