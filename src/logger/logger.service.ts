ba092ea996741b73602eff6a780f4009482e3db8
Blue Mouse <durexp@protonmail.ch>
2021-11-23 00:28:08 +0200

    logging only in dev env

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,5 +1,9 @@
 import { Logger } from '@nestjs/common'
 
 export class AppLoggerService extends Logger {
-
+  log(value, route) {
+    if (process.env.NODE_ENV === 'development') {
+      Logger.log(value, route)
+    }
+  }
 }
