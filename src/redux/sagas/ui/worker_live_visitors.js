6dd03dd9e46f27411470fbce9edfb1bbaffe43cc
Blue Mouse <durexp@protonmail.ch>
2021-12-01 02:16:19 +0200

    reduced 'live visitors' ping time

--- src/redux/sagas/ui/worker_live_visitors.js
+++ src/redux/sagas/ui/worker_live_visitors.js
@@ -5,11 +5,11 @@ import _isEmpty from 'lodash/isEmpty'
 import { getLiveVisitors } from 'api'
 import UIActions from 'redux/actions/ui'
 
-const LIVE_VISITORS_UPDATE_INTERVAL = 58000
+const LIVE_VISITORS_UPDATE_INTERVAL = 40000
 
 export default function* liveVisitors() {
   while (true) {
-    yield delay(2000)
+    yield delay(LIVE_VISITORS_UPDATE_INTERVAL)
     const projects = yield select(state => state.ui.projects.projects)
     if (_isEmpty(projects)) {
       continue
@@ -19,6 +19,5 @@ export default function* liveVisitors() {
     const liveStats = yield call(getLiveVisitors, pids)
 
     yield put(UIActions.setLiveStats(liveStats))
-    yield delay(LIVE_VISITORS_UPDATE_INTERVAL)
   }
 }
