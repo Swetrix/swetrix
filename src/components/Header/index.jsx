b841f5758812827a63bdb055ae7cf157f2294fc4
Blue Mouse <durexp@protonmail.ch>
2021-12-09 23:19:08 +0200

    updated logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -35,7 +35,7 @@ const Header = ({ authenticated }) => {
             <div className='flex items-center'>
               <Link to={routes.main}>
                 <span className='sr-only'>Swetrix</span>
-                <img className='h-10 w-28' src='/assets/logo_white.png' height='40' width='112' alt='' />
+                <img className='h-10' src='/assets/logo_white.svg' alt='' />
               </Link>
               <div className='hidden ml-10 space-x-1 lg:block'>
                 <NavLink to={routes.features} className='text-base select-none font-medium text-white hover:text-indigo-50 py-2 px-3 hover:bg-indigo-500 rounded-md' activeClassName='bg-indigo-700 hover:bg-indigo-700' key='Features'>
@@ -151,7 +151,7 @@ const Header = ({ authenticated }) => {
               <div className="flex items-center justify-between">
                 <Link to={routes.main}>
                   <span className='sr-only'>Swetrix</span>
-                  <img className='h-10 w-28' src='/assets/logo_blue.png' height='40' width='112' alt='' />
+                  <img className='h-10' src='/assets/logo_blue.svg' alt='' />
                 </Link>
                 <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                   <span className="sr-only">
