ac9810f1f6d15e9d0f3651038d5853e973f0e922
Blue Mouse <durexp@protonmail.ch>
2021-12-16 01:06:14 +0200

    fixed codestyling

--- src/api/index.js
+++ src/api/index.js
@@ -1,227 +1,227 @@
-import axios from "axios";
-import { store } from "redux/store";
-import Debug from "debug";
-import _map from "lodash/map";
-import _isEmpty from "lodash/isEmpty";
-import { authActions } from "redux/actions/auth";
+import axios from 'axios'
+import { store } from 'redux/store'
+import Debug from 'debug'
+import _map from 'lodash/map'
+import _isEmpty from 'lodash/isEmpty'
+import { authActions } from 'redux/actions/auth'
 
-import { getAccessToken, removeAccessToken } from "utils/accessToken";
-import { isSelfhosted } from "redux/constants";
+import { getAccessToken, removeAccessToken } from 'utils/accessToken'
+import { isSelfhosted } from 'redux/constants'
 
-const debug = Debug("swetrix:api");
+const debug = Debug('swetrix:api')
 
 const api = axios.create({
   baseURL: isSelfhosted ? window.env.API_URL : process.env.REACT_APP_API_URL,
-});
+})
 
 api.interceptors.request.use(
   (config) => {
-    const token = getAccessToken();
+    const token = getAccessToken()
     if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
+      config.headers['Authorization'] = `Bearer ${token}`
     }
-    return config;
+    return config
   },
   (error) => {
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
 api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.data.statusCode === 401) {
-      removeAccessToken();
-      store.dispatch(authActions.logout());
+      removeAccessToken()
+      store.dispatch(authActions.logout())
     }
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
 export const authMe = () =>
   api
-    .get("/auth/me")
+    .get('/auth/me')
     .then((response) => response.data)
     .catch((error) => {
-      debug("%s", error);
+      debug('%s', error)
       throw _isEmpty(error.response.data?.message)
         ? error.response.data
-        : error.response.data.message;
-    });
+        : error.response.data.message
+    })
 
 export const login = (credentials) =>
   api
-    .post("/auth/login", credentials)
+    .post('/auth/login', credentials)
     .then((response) => response.data)
     .catch((error) => {
-      debug("%s", error);
+      debug('%s', error)
       throw _isEmpty(error.response.data?.message)
         ? error.response.data
-        : error.response.data.message;
-    });
+        : error.response.data.message
+    })
 
 export const signup = (data) =>
   api
-    .post("/auth/register", data)
+    .post('/auth/register', data)
     .then((response) => response.data)
     .catch((error) => {
-      const errorsArray = error.response.data.message;
+      const errorsArray = error.response.data.message
       if (Array.isArray(errorsArray)) {
-        throw errorsArray;
+        throw errorsArray
       }
-      throw new Error(errorsArray);
-    });
+      throw new Error(errorsArray)
+    })
 
 export const deleteUser = () =>
   api
-    .delete("/user")
+    .delete('/user')
     .then((response) => response.data)
     .catch((error) => {
-      throw new Error(JSON.stringify(error.response.data));
-    });
+      throw new Error(JSON.stringify(error.response.data))
+    })
 
 export const changeUserDetails = (data) =>
   api
-    .put("/user", data)
+    .put('/user', data)
     .then((response) => response.data)
     .catch((error) => {
-      const errorsArray = error.response.data.message;
+      const errorsArray = error.response.data.message
       if (Array.isArray(errorsArray)) {
-        throw errorsArray;
+        throw errorsArray
       }
-      throw new Error(errorsArray);
-    });
+      throw new Error(errorsArray)
+    })
 
 export const upgradePlan = (planCode) =>
-  api.post("/user/upgrade", { planCode }).catch((error) => {
+  api.post('/user/upgrade', { planCode }).catch((error) => {
     if (error?.response) {
-      const errorsArray = error.response.data.message;
+      const errorsArray = error.response.data.message
       if (Array.isArray(errorsArray)) {
-        throw errorsArray;
+        throw errorsArray
       }
-      throw new Error(errorsArray);
+      throw new Error(errorsArray)
     } else {
-      throw new Error(error);
+      throw new Error(error)
     }
-  });
+  })
 
 export const forgotPassword = (email) =>
   api
-    .post("/auth/reset-password", email)
+    .post('/auth/reset-password', email)
     .then((response) => response.data)
     .catch((error) => {
-      debug("%s", error);
+      debug('%s', error)
       throw _isEmpty(error.response.data?.message)
         ? error.response.data
-        : error.response.data.message;
-    });
+        : error.response.data.message
+    })
 
 export const confirmEmail = () =>
   api
-    .post("/user/confirm_email")
+    .post('/user/confirm_email')
     .then((response) => response.data)
     .catch((error) => {
-      debug("%s", error);
+      debug('%s', error)
       throw _isEmpty(error.response.data?.message)
         ? error.response.data
-        : error.response.data.message;
-    });
+        : error.response.data.message
+    })
 
 export const exportUserData = () =>
   api
-    .get("/user/export")
+    .get('/user/export')
     .then((response) => response.data)
     .catch((error) => {
-      debug("%s", error);
+      debug('%s', error)
       throw _isEmpty(error.response.data?.message)
         ? error.response.data
-        : error.response.data.message;
-    });
+        : error.response.data.message
+    })
 
 export const createNewPassword = (id, password) =>
   api
     .post(`/auth/password-reset/${id}`, { password })
     .then((response) => response.data)
     .catch((error) => {
-      const errorsArray = error.response.data.message;
+      const errorsArray = error.response.data.message
       if (Array.isArray(errorsArray)) {
-        throw errorsArray;
+        throw errorsArray
       }
-      throw new Error(errorsArray);
-    });
+      throw new Error(errorsArray)
+    })
 
 export const verifyEmail = ({ path, id }) =>
   api
     .get(`/auth/${path}/${id}`)
     .then((response) => response.data)
     .catch((error) => {
-      debug("%s", error);
+      debug('%s', error)
       throw _isEmpty(error.response.data?.message)
         ? error.response.data
-        : error.response.data.message;
-    });
+        : error.response.data.message
+    })
 
 export const getProjects = () =>
   api
-    .get("/project")
+    .get('/project')
     .then((response) => response.data)
     .catch((error) => {
-      debug("%s", error);
+      debug('%s', error)
       throw _isEmpty(error.response.data?.message)
         ? error.response.data
-        : error.response.data.message;
-    });
+        : error.response.data.message
+    })
 
 export const getProject = (pid) =>
   api
     .get(`/project/${pid}`)
     .then((response) => response.data)
     .catch((error) => {
-      debug("%s", error);
+      debug('%s', error)
       throw _isEmpty(error.response.data?.message)
         ? error.response.data
-        : error.response.data.message;
-    });
+        : error.response.data.message
+    })
 
 export const createProject = (data) =>
   api
     .post(`/project`, data)
     .then((response) => response.data)
     .catch((error) => {
-      debug("%s", error);
+      debug('%s', error)
       throw _isEmpty(error.response.data?.message)
         ? error.response.data
-        : error.response.data.message;
-    });
+        : error.response.data.message
+    })
 
 export const updateProject = (id, data) =>
   api
     .put(`/project/${id}`, data)
     .then((response) => response.data)
     .catch((error) => {
-      debug("%s", error);
+      debug('%s', error)
       throw _isEmpty(error.response.data?.message)
         ? error.response.data
-        : error.response.data.message;
-    });
+        : error.response.data.message
+    })
 
 export const deleteProject = (id) =>
   api
     .delete(`/project/${id}`)
     .then((response) => response.data)
     .catch((error) => {
-      debug("%s", error);
+      debug('%s', error)
       throw _isEmpty(error.response.data?.message)
         ? error.response.data
-        : error.response.data.message;
-    });
+        : error.response.data.message
+    })
 
 export const getProjectData = (
   pid,
-  tb = "hour",
-  period = "3d",
-  from = "",
-  to = ""
+  tb = 'hour',
+  period = '3d',
+  from = '',
+  to = ''
 ) =>
   api
     .get(
@@ -229,30 +229,30 @@ export const getProjectData = (
     )
     .then((response) => response.data)
     .catch((error) => {
-      debug("%s", error);
+      debug('%s', error)
       throw _isEmpty(error.response.data?.message)
         ? error.response.data
-        : error.response.data.message;
-    });
+        : error.response.data.message
+    })
 
 export const getOverallStats = (pids) =>
   api
-    .get(`log/birdseye?pids=[${_map(pids, (pid) => `"${pid}"`).join(",")}]`)
+    .get(`log/birdseye?pids=[${_map(pids, (pid) => `"${pid}"`).join(',')}]`)
     .then((response) => response.data)
     .catch((error) => {
-      debug("%s", error);
+      debug('%s', error)
       throw _isEmpty(error.response.data?.message)
         ? error.response.data
-        : error.response.data.message;
-    });
+        : error.response.data.message
+    })
 
 export const getLiveVisitors = (pids) =>
   api
-    .get(`log/hb?pids=[${_map(pids, (pid) => `"${pid}"`).join(",")}]`)
+    .get(`log/hb?pids=[${_map(pids, (pid) => `"${pid}"`).join(',')}]`)
     .then((response) => response.data)
     .catch((error) => {
-      debug("%s", error);
+      debug('%s', error)
       throw _isEmpty(error.response.data?.message)
         ? error.response.data
-        : error.response.data.message;
-    });
+        : error.response.data.message
+    })
