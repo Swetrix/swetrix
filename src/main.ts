499c2cefeeeee2b4d81209ec0ff9a2de175a2c61
Blue Mouse <durexp@protonmail.ch>
2021-12-30 20:34:59 +0200

    fixed f***ing production cors issue

--- src/main.ts
+++ src/main.ts
@@ -4,34 +4,31 @@ import * as cookieParser from 'cookie-parser'
 import * as bodyParser from 'body-parser'
 
 import { AppModule } from './app.module'
-import { isSelfhosted } from './common/constants'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.use(cookieParser())
   app.useGlobalPipes(new ValidationPipe())
 
-  if (isSelfhosted) {
-    app.use(async (req, res, next) => {
-      res.header('Cross-Origin-Embedder-Policy', 'require-corp; report-to=\'default\'')
-      res.header('Cross-Origin-Opener-Policy', 'same-site; report-to=\'default\'')
-      res.header('Cross-Origin-Resource-Policy', 'same-site')
-      res.header('Permissions-Policy', 'interest-cohort=()')
-      res.header('Referrer-Policy', 'strict-origin-when-cross-origin')
-      res.header('X-Frame-Options', 'DENY')
-      res.header('X-Powered-By', 'Mountain Dew')
-      res.header('X-XSS-Protection', '1; mode=block')
-      res.header('Access-Control-Allow-Origin', process.env.API_ORIGINS || '*')
-      res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
-      res.header('Access-Control-Allow-Headers', '*')
-  
-      if (req.method === 'OPTIONS') {
-        return await res.sendStatus(204)
-      }
-  
-      next()
-    })
-  }
+  app.use(async (req, res, next) => {
+    res.header('Cross-Origin-Embedder-Policy', 'require-corp; report-to=\'default\'')
+    res.header('Cross-Origin-Opener-Policy', 'same-site; report-to=\'default\'')
+    res.header('Cross-Origin-Resource-Policy', 'same-site')
+    res.header('Permissions-Policy', 'interest-cohort=()')
+    res.header('Referrer-Policy', 'strict-origin-when-cross-origin')
+    res.header('X-Frame-Options', 'DENY')
+    res.header('X-Powered-By', 'Mountain Dew')
+    res.header('X-XSS-Protection', '1; mode=block')
+    res.header('Access-Control-Allow-Origin', process.env.API_ORIGINS || '*')
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+    res.header('Access-Control-Allow-Headers', '*')
+
+    if (req.method === 'OPTIONS') {
+      return await res.sendStatus(204)
+    }
+
+    next()
+  })
 
   app.use('/webhook', bodyParser.raw({ type: 'application/json' }))
   await app.listen(5005)
