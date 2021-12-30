43a6c55d00db321aa156da7605c9209b64091c16
Blue Mouse <durexp@protonmail.ch>
2021-12-30 12:23:28 +0200

    fixed Progress dark theme colours

--- src/ui/Progress.jsx
+++ src/ui/Progress.jsx
@@ -2,7 +2,7 @@ import React, { memo } from 'react'
 
 const Progress = ({ now }) => (
   <div className='relative'>
-    <div className='overflow-hidden h-2 text-xs flex rounded bg-blue-200'>
+    <div className='overflow-hidden h-2 text-xs flex rounded bg-blue-200 dark:bg-gray-400'>
       <div style={{ width: `${now}%` }} className='shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500 dark:bg-blue-700' />
     </div>
   </div>
