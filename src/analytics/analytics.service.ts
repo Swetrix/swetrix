db201e78502f1ab3e0bdd88113171cb0eaedc847
Blue Mouse <durexp@protonmail.ch>
2022-02-11 22:07:28 +0200

    fix locale validation

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -186,7 +186,7 @@ export class AnalyticsService {
       // TODO: IMPORTANT!: validate pg param
 
       // validate locale ('lc' param)
-      if (_isEmpty(lc)) {
+      if (!_isEmpty(lc)) {
         if (isLocale(lc)) {
           // uppercase the locale after '-' char, so for example both 'en-gb' and 'en-GB' in result will be 'en-GB'
           const lcParted = _split(lc, '-')
