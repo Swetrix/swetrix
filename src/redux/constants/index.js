957a47d7a6618bb3dffbe27619a6c5a51551de84
Blue Mouse <durexp@protonmail.ch>
2022-04-08 03:08:42 +0300

    added linkedin url

--- src/redux/constants/index.js
+++ src/redux/constants/index.js
@@ -51,6 +51,8 @@ export const DONATE_URL = 'https://ko-fi.com/andriir'
 export const FIREFOX_ADDON_URL = 'https://addons.mozilla.org/en-US/firefox/addon/swetrix/'
 export const CHROME_EXTENSION_URL = 'https://chrome.google.com/webstore/detail/swetrix/glbeclfdldjldjonfnpnembfkhphmeld'
 export const HAVE_I_BEEN_PWNED_URL = 'https://haveibeenpwned.com/passwords'
+export const LINKEDIN_URL = 'https://www.linkedin.com/company/swetrix/'
+export const GITHUB_URL = 'https://github.com/Swetrix'
 
 export const CONTACT_EMAIL = 'contact@swetrix.com'
 export const SECURITY_EMAIL = 'security@swetrix.com'
