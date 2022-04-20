e751f6050947a4f4d498f077dcf019867f0079e0
Blue Mouse <durexp@protonmail.ch>
2022-04-20 03:19:44 +0300

    don't generate PIDs with the '-' character

--- src/utils/random.js
+++ src/utils/random.js
@@ -1,5 +1,5 @@
 import { customAlphabet } from 'nanoid'
 
-const legalCharacters = '1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm-'
+const legalCharacters = '1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'
 
 export const nanoid = customAlphabet(legalCharacters, 12)
