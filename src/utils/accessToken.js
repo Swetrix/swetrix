45eadf8e4990bc805057167901794aa1ce4809ca
kruzhambus <maks.mrug.ru@gmail.com>
2022-06-20 22:07:35 +0000

    save a token in coockie for working with extensions

--- src/utils/accessToken.js
+++ src/utils/accessToken.js
@@ -1,27 +1,15 @@
-import _isEmpty from 'lodash/isEmpty'
 import { TOKEN } from 'redux/constants'
-import { getItem, setItem, removeItem } from './localstorage'
+import { setCookie, getCookie } from './cookie'
 
 export const getAccessToken = () => {
-  let accessToken = getItem(TOKEN)
-
-  if (_isEmpty(accessToken)) {
-    accessToken = sessionStorage.getItem(TOKEN)
-  }
-
-  return accessToken
+  return getCookie(TOKEN)
 }
 
-export const setAccessToken = (token, temporary = false) => {
-  if (temporary) {
-    sessionStorage.setItem(TOKEN, token)
-    return
-  }
-
-  setItem(TOKEN, token)
+export const setAccessToken = (token) => {
+  setCookie(TOKEN, token)
 }
 
 export const removeAccessToken = () => {
-  removeItem(TOKEN)
+  setCookie(TOKEN, '', 0)
   sessionStorage.removeItem(TOKEN)
 }
