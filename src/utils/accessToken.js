04e728a63aea457c8784da33f107a3cb68ed39e7
kruzhambus <maks.mrug.ru@gmail.com>
2022-06-21 08:36:04 +0000

    add session storage

--- src/utils/accessToken.js
+++ src/utils/accessToken.js
@@ -1,11 +1,24 @@
+import _isEmpty from 'lodash/isEmpty'
 import { TOKEN } from 'redux/constants'
-import { setCookie, getCookie } from './cookie'
+import { getCookie, setCookie } from './cookie'
 
+// It's important to first check in local storage, then in session storage, not vice versa.
 export const getAccessToken = () => {
-  return getCookie(TOKEN)
+  let accessToken = getCookie(TOKEN)
+
+  if (_isEmpty(accessToken)) {
+    accessToken = sessionStorage.getItem(TOKEN)
+  }
+
+  return accessToken
 }
 
-export const setAccessToken = (token) => {
+export const setAccessToken = (token, temporary = false) => {
+  if (temporary) {
+    sessionStorage.setItem(TOKEN, token)
+    return
+  }
+
   setCookie(TOKEN, token)
 }
 
