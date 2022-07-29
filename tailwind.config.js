e8c7451433da60a097fbf7520022647a3245f1cc
kruzhambus <maks.mrug.ru@gmail.com>
2022-07-29 09:55:17 -0400

    added to tailwind config all responsive size

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
   theme: {
     screens: {
       'xs': '400px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     },
     extend: {
       colors: {
