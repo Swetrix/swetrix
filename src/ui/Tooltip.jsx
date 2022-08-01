eafff266a495277aed3580b1644cfdda8fa7d12b
kruzhambus <maks.mrug.ru@gmail.com>
2022-08-01 16:46:55 -0400

    fixed

--- src/ui/Tooltip.jsx
+++ src/ui/Tooltip.jsx
@@ -10,11 +10,11 @@ const Tooltip = ({
     {tooltipNode || (
       <QuestionMarkCircleIcon className='w-5 h-5 text-gray-700  dark:text-gray-300' />
     )}
-    <div className='absolute bottom-0 right-0 flex-col mb-6 hidden group-hover:flex items-end'>
+    <div className='absolute bottom-0 -mr-12 right-0 flex-col mb-6 hidden group-hover:flex items-end'>
       <span className='relative w-60 z-10 p-2 text-xs leading-none text-white whitespace-no-wrap bg-gray-700 shadow-lg rounded-md'>
         {text}
       </span>
-      <div className='mr-1 w-3 h-3 -mt-2 rotate-45 bg-gray-700' />
+      <div className='w-3 h-3 -mt-2 rotate-45 bg-gray-700 mr-[52px]' />
     </div>
   </div>
 )
