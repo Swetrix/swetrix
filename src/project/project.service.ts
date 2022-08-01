ca226fb7fd536918f649302eaaf3d2a9c6c5d8f5
Yevhenii K <109991141+rhaxma@users.noreply.github.com>
2022-08-01 23:01:57 +0200

    Added restriction on the black list of IP addresses

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -219,6 +219,7 @@ export class ProjectService {
     if (_size(projectDTO.name) > 50) throw new UnprocessableEntityException('The project name is too long')
     if (!_isArray(projectDTO.origins)) throw new UnprocessableEntityException('The list of allowed origins has to be an array of strings')
     if (_size(_join(projectDTO.origins, ',')) > 300) throw new UnprocessableEntityException('The list of allowed origins has to be smaller than 300 symbols')
+    if (_size(_join(projectDTO.ipBlacklist, ',')) > 300) throw new UnprocessableEntityException('The list of allowed blacklisted IP addresses must be less than 300 characters.')
   }
 
   // Returns amount of existing events starting from month
@@ -255,7 +256,7 @@ export class ProjectService {
       const customEvents = (await clickhouse.query(count_custom_ev_query).toPromise())[0]['count()']
 
       count = pageviews + customEvents
-      
+
       await redis.set(countKey, `${pageviews}`, 'EX', redisProjectCountCacheTimeout)
     } else {
       try {
