0d7111456396547ac4ebfa162bfd14b04b2fea67
Blue Mouse <durexp@protonmail.ch>
2022-08-06 02:15:07 +0300

    added some integrations

--- src/pages/Docs/Docs.jsx
+++ src/pages/Docs/Docs.jsx
@@ -51,6 +51,29 @@ const contents = (t) => [{
 
 const NEXTJS_REPO_URL = 'https://github.com/Swetrix/swetrix-nextjs'
 
+const integrations = [
+  {
+    name: 'Next.js',
+    link: NEXTJS_REPO_URL,
+  },
+  {
+    name: 'Python Django',
+    link: 'https://github.com/Swetrix/integrations/tree/main/django',
+  },
+  {
+    name: 'Webflow',
+    link: 'https://github.com/Swetrix/integrations/tree/main/webflow',
+  },
+  {
+    name: 'Wordpress',
+    link: 'https://github.com/Swetrix/integrations/tree/main/wordpress',
+  },
+  {
+    name: 'Wix',
+    link: 'https://github.com/Swetrix/integrations/tree/main/wix',
+  },
+]
+
 const Contents = ({ t }) => {
   const tContents = contents(t)
 
@@ -136,11 +159,13 @@ const Docs = () => {
               :
             </p>
             <ul className='ml-5 text-lg list-disc marker:text-gray-900 dark:marker:text-gray-50'>
-              <li>
-                <a href={NEXTJS_REPO_URL} className='flex hover:underline hover:opacity-80 text-indigo-600 dark:text-indigo-400 ml-1' target='_blank' rel='noopener noreferrer'>
-                  Next.js
-                </a>
-              </li>
+              {_map(integrations, ({ name, link }) => (
+                <li key={name}>
+                  <a href={link} className='flex hover:underline hover:opacity-80 text-indigo-600 dark:text-indigo-400 ml-1' target='_blank' rel='noopener noreferrer'>
+                    {name}
+                  </a>
+                </li>
+              ))}
             </ul>
 
             <CHeader id='docs-ht' name={t('docs.titles.how')} />
