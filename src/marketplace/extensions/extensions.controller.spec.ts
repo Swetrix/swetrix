17af6e596c2941ad5d2b527d0049b2404d02e503
YakovAkk <james.yasko.2002@gmail.com>
2022-08-31 21:00:51 +0300

    was fixed all tests

--- src/marketplace/extensions/extensions.controller.spec.ts
+++ src/marketplace/extensions/extensions.controller.spec.ts
@@ -1,18 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Extension } from './extension.entity'
 import { ExtensionsController } from './extensions.controller'
+import { ExtensionsService } from './extensions.service'
 
 describe('ExtensionsController', () => {
   let controller: ExtensionsController
-
+  const EXTENSION_REPOSITORY_TOKEN = getRepositoryToken(Extension)
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ExtensionsController],
+      providers: [
+        ExtensionsService,
+        {
+          provide: EXTENSION_REPOSITORY_TOKEN,
+          useValue: { create: jest.fn(), save: jest.fn(), findOne: jest.fn() },
+        },
+      ],
     }).compile()
 
     controller = module.get<ExtensionsController>(ExtensionsController)
   })
-
-  it('should be defined', () => {
-    expect(controller).toBeDefined()
+  describe('root', () => {
+    it('should be defined', () => {
+      expect(controller).toBeDefined()
+    })
   })
 })
