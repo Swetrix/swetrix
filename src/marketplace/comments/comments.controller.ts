bcfb59326998b4a8f1162ee2de6efd7be5c2481d
Yevhenii Kulisidi <109991141+rhaxma@users.noreply.github.com>
2022-10-16 01:17:22 +0200

    added route for getting all extension comments

--- src/marketplace/comments/comments.controller.ts
+++ src/marketplace/comments/comments.controller.ts
@@ -1,6 +1,32 @@
-import { Controller } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import { Controller, Get, Query } from '@nestjs/common'
+import { ApiQuery, ApiTags } from '@nestjs/swagger'
+import { CommentsService } from './comments.service'
+import { GetCommentsQueryDto } from './dtos/queries/get-comments.dto'
+import { Comment } from './entities/comment.entity'
 
 @ApiTags('comments')
 @Controller('comments')
-export class CommentsController {}
+export class CommentsController {
+  constructor(private readonly commentsService: CommentsService) {}
+
+  @Get()
+  @ApiQuery({ name: 'offset', required: false, type: String })
+  @ApiQuery({ name: 'limit', required: false, type: String })
+  @ApiQuery({ name: 'extensionId', required: false, type: String })
+  @ApiQuery({ name: 'userId', required: false, type: String })
+  async getComments(@Query() queries: GetCommentsQueryDto): Promise<{
+    comments: Comment[]
+    count: number
+  }> {
+    const [comments, count] = await this.commentsService.findAndCount({
+      where: {
+        ...(queries.extensionId && { extensionId: queries.extensionId }),
+        ...(queries.userId && { userId: queries.userId }),
+      },
+      skip: queries.offset || 0,
+      take: queries.limit || 25,
+    })
+
+    return { comments, count }
+  }
+}
--- src/marketplace/comments/comments.service.ts
+++ src/marketplace/comments/comments.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { FindManyOptions, Repository } from 'typeorm'
+import { Comment } from './entities/comment.entity'
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(
+    @InjectRepository(Comment) private commentsRepository: Repository<Comment>,
+  ) {}
+
+  async findAndCount(
+    options: FindManyOptions<Comment>,
+  ): Promise<[Comment[], number]> {
+    return await this.commentsRepository.findAndCount({ ...options })
+  }
+}
