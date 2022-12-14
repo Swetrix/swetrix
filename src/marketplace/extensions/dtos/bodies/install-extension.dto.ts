9d11da4575e415c311f8bb7632ea274513e0a6aa
kruzhambus <maks.mrug.ru@gmail.com>
2022-12-15 00:34:53 +0200

    fix: install extension with projectId

--- src/marketplace/extensions/dtos/bodies/install-extension.dto.ts
+++ src/marketplace/extensions/dtos/bodies/install-extension.dto.ts
@@ -1,7 +1,6 @@
-import { IsUUID, ValidateIf } from 'class-validator'
+import { ValidateIf } from 'class-validator'
 
 export class InstallExtensionBodyDto {
-  @IsUUID()
   @ValidateIf((_object, value) => Boolean(value))
   readonly projectId: string | null
 }
