c14684ca41caea36affa340a2c6b845a23b8ac47
Blue Mouse <durexp@protonmail.ch>
2022-12-29 04:35:28 +0000

    fix tab colours for dark theme

--- src/pages/Project/View/ViewProject.jsx
+++ src/pages/Project/View/ViewProject.jsx
@@ -865,15 +865,15 @@ const ViewProject = ({
                         key={tab.id}
                         className={cx(
                           isCurrent
-                            ? 'border-indigo-500 text-indigo-700'
-                            : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+                            ? 'border-indigo-700 text-indigo-700 dark:text-indigo-500 dark:border-indigo-500'
+                            : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 dark:text-gray-400 dark:hover:border-gray-300 dark:hover:text-gray-300',
                           'group inline-flex items-center whitespace-nowrap py-2 px-1 border-b-2 font-bold text-md cursor-pointer',
                         )}
                         aria-current={isCurrent ? 'page' : undefined}
                       >
                         <tab.icon
                           className={cx(
-                            isCurrent ? 'text-indigo-700' : 'text-gray-400 group-hover:text-gray-500',
+                            isCurrent ? 'text-indigo-700 dark:text-indigo-500' : 'text-gray-500 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-300',
                             '-ml-0.5 mr-2 h-5 w-5',
                           )}
                           aria-hidden='true'
