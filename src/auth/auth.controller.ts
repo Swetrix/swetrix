e006bdba5b0282494ab03e702f9a7eed1d547031
Yevhenii Kulisidi <109991141+yevheniikulisidi@users.noreply.github.com>
2023-01-06 10:36:39 +0100

    Added filter and pipe to controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,21 @@
-import { Controller } from '@nestjs/common'
+import {
+  Controller,
+  UseFilters,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
+import { I18nValidationExceptionFilter } from 'nestjs-i18n'
 
 @ApiTags('Auth')
 @Controller('auth')
+@UseFilters(new I18nValidationExceptionFilter())
+@UsePipes(
+  new ValidationPipe({
+    forbidNonWhitelisted: true,
+    forbidUnknownValues: true,
+    transform: true,
+    whitelist: true,
+  }),
+)
 export class AuthController {}
