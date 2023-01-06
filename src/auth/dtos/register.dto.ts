f636599cc0cf0794bec9f5323748ef5aa40a6294
Yevhenii Kulisidi <109991141+yevheniikulisidi@users.noreply.github.com>
2023-01-06 09:45:04 +0100

    Added validation for registration fields

--- src/auth/dtos/register.dto.ts
+++ src/auth/dtos/register.dto.ts
@@ -1,7 +1,17 @@
+import { IsBoolean, IsEmail, IsNotEmpty, Matches } from 'class-validator'
+
 export namespace RegisterDto {
   export class Request {
+    @IsEmail()
     public readonly email: string
+
+    @Matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,72}$/,
+    )
     public readonly password: string
+
+    @IsNotEmpty()
+    @IsBoolean()
     public readonly checkIfLeaked: boolean
   }
 
