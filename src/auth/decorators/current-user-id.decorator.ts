8b36be61fa0647e79b2262828d9ca3a14cca1488
Yevhenii Kulisidi <109991141+yevheniikulisidi@users.noreply.github.com>
2023-01-10 16:40:10 +0100

    Fixed decorator with getting current user ID

--- src/auth/decorators/current-user-id.decorator.ts
+++ src/auth/decorators/current-user-id.decorator.ts
@@ -5,6 +5,9 @@ export const CurrentUserId = createParamDecorator(
   (_: unknown, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest()
     const user = request.user as IJwtPayload
+
+    if (!user) return null
+
     return user.sub
   },
 )
