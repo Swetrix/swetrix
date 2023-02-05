f4703137750335965bca92c9359fd783fce3138c
Blue Mouse <durexp@protonmail.ch>
2023-02-05 15:24:29 +0000

    improved dark theme background colour

--- src/ui/Progress.jsx
+++ src/ui/Progress.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Progress = ({ now }) => (
   <div className='relative'>
-    <div className='overflow-hidden h-2 text-xs flex rounded bg-blue-200 dark:bg-gray-400'>
+    <div className='overflow-hidden h-2 text-xs flex rounded bg-blue-200 dark:bg-gray-500'>
       <div style={{ width: `${now}%` }} className='shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500 dark:bg-blue-700' />
     </div>
   </div>
