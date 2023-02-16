d7c2be09d873be24eba3fd7ac311089318a1806b
Blue Mouse <durexp@protonmail.ch>
2023-02-16 22:02:53 +0000

    some improvements

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -141,6 +141,15 @@ const Header = ({
                     {status}
                   </Link>
                 )}
+                {authenticated && user?.planCode === 'none' && (
+                  <Link
+                    to={routes.billing}
+                    className='flex justify-center items-center text-base select-none font-medium py-2 px-2 rounded-md text-rose-800 bg-rose-200 dark:bg-rose-300 hover:bg-rose-300 dark:hover:bg-rose-200'
+                    key='NoSubscription'
+                  >
+                    {t('billing.inactive')}
+                  </Link>
+                )}
                 <a href={BLOG_URL} className='flex justify-center items-center text-base select-none font-medium text-white hover:text-indigo-50 py-2 px-2 dark:hover:bg-gray-700 hover:bg-indigo-500 rounded-md' target='_blank' rel='noreferrer noopener'>
                   <RssIcon className='w-5 h-5 mr-1' />
                   {t('footer.blog')}
