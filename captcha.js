d8e399f0ab6354e27ab48d58a1a3fc0a675f17b6
Blue Mouse <durexp@protonmail.ch>
2023-03-02 22:44:05 +0000

    intial design

---
+++ captcha.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const captchaBtn = document.querySelector('#challenge')
+
+  captchaBtn.addEventListener('click', () => {
+    console.log('clicked')
+  })
+})
