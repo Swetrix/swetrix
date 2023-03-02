bb5eed938be991bd33e58990068255a4ffcc0597
Blue Mouse <durexp@protonmail.ch>
2023-03-02 23:44:27 +0000

    ui: add action statuses

--- captcha.js
+++ captcha.js
@@ -1,7 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const captchaBtn = document.querySelector('#challenge')
+/**
+ * Sets the provided action visible and the rest hidden
+ * @param {*} action checkbox | failure | completed | loading
+ */
+const activateAction = (action) => {
+  const actions = {
+    checkbox: document.querySelector('#checkbox'),
+    failure: document.querySelector('#failure'),
+    completed: document.querySelector('#completed'),
+    loading: document.querySelector('#loading'),
+  }
 
-  captchaBtn.addEventListener('click', () => {
-    console.log('clicked')
+  // Apply hidden class to all actions
+  actions.checkbox.classList.add('hidden')
+  actions.failure.classList.add('hidden')
+  actions.completed.classList.add('hidden')
+  actions.loading.classList.add('hidden')
+
+  // Remove hidden class from the provided action
+  actions[action].classList.remove('hidden')
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const captchaComponent = document.querySelector('#swetrix-captcha')
+  const branding = document.querySelector('#branding')
+
+  branding.addEventListener('click', (e) => {
+    e.stopPropagation()
+  })
+
+  captchaComponent.addEventListener('click', () => {
+    activateAction('loading')
+
+    setTimeout(() => {
+      activateAction('completed')
+      // activateAction('failure')
+    }, 2000)
   })
 })
