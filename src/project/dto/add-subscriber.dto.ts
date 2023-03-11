0ea56d049d7bb6d4d54cd2b4f565dffff532eccf
Yevhenii Kulisidi <109991141+yevheniikulisidi@users.noreply.github.com>
2023-03-12 00:39:57 +0200

    Fixed DTO of adding subscriber

--- src/project/dto/add-subscriber.dto.ts
+++ src/project/dto/add-subscriber.dto.ts
@@ -4,13 +4,13 @@ import { ReportFrequency } from '../enums'
 export class AddSubscriberParamsDto {
   @Matches(/^(?!.*--)[a-zA-Z0-9-]{12}$/)
   readonly projectId: string
-
-  @IsNotEmpty()
-  @IsEnum(ReportFrequency)
-  readonly reportFrequency: ReportFrequency
 }
 
 export class AddSubscriberBodyDto {
   @IsEmail()
   readonly email: string
+
+  @IsNotEmpty()
+  @IsEnum(ReportFrequency)
+  readonly reportFrequency: ReportFrequency
 }
