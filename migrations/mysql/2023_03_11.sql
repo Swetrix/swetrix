f45b441afd0a1511ccebaf5d1d0b9841c4675998
Blue Mouse <durexp@protonmail.ch>
2023-03-11 18:28:15 +0000

    add migrations

--- migrations/mysql/2023_03_11.sql
+++ migrations/mysql/2023_03_11.sql
@@ -1,2 +1,5 @@
 alter table extension modify column `version` varchar(255) NOT NULL DEFAULT '0.0.1' AFTER description;
 alter table extension modify column `status` enum('ACCEPTED','REJECTED','PENDING','NO_EXTENSION_UPLOADED') NOT NULL AFTER version;
+
+alter table extension_to_project add column `createdAt` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) AFTER isActive;
+alter table extension add column `tags` text NOT NULL AFTER fileURL;
