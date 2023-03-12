baef3cf895301ac6717936289dd616864b75a9b7
Blue Mouse <durexp@protonmail.ch>
2023-03-12 02:00:43 +0000

    got rid of browser builds and uglify package

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 import typescript from 'rollup-plugin-typescript2'
-import { uglify } from 'rollup-plugin-uglify'
 import pkg from './package.json'
 
 export default [
@@ -11,19 +10,9 @@ export default [
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' },
-      { file: pkg.origbrowser, format: 'umd', name: 'captcha-validator' },
     ],
     plugins: [
       typescript(), nodeResolve(), commonjs(), sourceMaps(),
     ]
   },
-  {
-    input: 'src/index.ts',
-    output: [
-      { file: pkg.browser, format: 'umd', name: 'captcha-validator' },
-    ],
-    plugins: [
-      typescript(), nodeResolve(), commonjs(), uglify(),
-    ]
-  }
 ]
