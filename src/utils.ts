6e9887f7e71d019d94e22dd6d89a106e336be10f
Blue Mouse <durexp@protonmail.ch>
2023-03-12 02:00:16 +0000

    fetch -> https

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import https from 'https'
+
 const DEFAULT_API_HOST = 'https://api.swetrix.com/captcha'
 
 export const ENDPOINTS = {
@@ -5,19 +7,50 @@ export const ENDPOINTS = {
 }
 
 export const makeAPIRequest = async (path: string, method: string, body: object, apiURL?: string): Promise<any> => {
-  let res
-
-  try {
-    res = await fetch(`${apiURL || DEFAULT_API_HOST}${path}`, {
-      method,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    })
-  } catch (e) {
-    throw `Unable to make API request, error: ${e}`
+  const options = {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
   }
 
-  return await res.json()
+  if (body) {
+    // @ts-ignore
+    options.body = JSON.stringify(body)
+  }
+
+  return new Promise((resolve, reject) => {
+    const url = `${apiURL || DEFAULT_API_HOST}${path}`
+
+    const req = https.request(url, options, (res: any) => {
+      let data = ''
+
+      res.on('data', (chunk: any) => {
+        data += chunk
+      })
+
+      res.on('end', () => {
+        if (res.statusCode >= 400) {
+          reject(`Request failed with status code ${res.statusCode}: ${data}`)
+        } else {
+          try {
+            resolve(JSON.parse(data))
+          } catch (e) {
+            reject(`Unable to parse JSON response: ${data}`)
+          }
+        }
+      })
+    })
+
+    req.on('error', (e) => {
+      reject(`Unable to make API request, error: ${e}`)
+    })
+
+    if (body) {
+      // @ts-ignore
+      req.write(options.body)
+    }
+
+    req.end()
+  })
 }
