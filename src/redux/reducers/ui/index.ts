2b877ec41fc1a21383ad9e7f9fc61d3d34067102
kruzhambus <maks.mrug.ru@gmail.com>
2023-03-24 23:32:46 +0200

    redux/ui/misc complete ✅

--- src/redux/reducers/ui/index.ts
+++ src/redux/reducers/ui/index.ts
@@ -1,9 +1,11 @@
 import { alertsActions } from './alerts'
 import { cacheActions } from './cache'
+import { miscActions } from './misc'
 
 const UIActions = {
   ...alertsActions,
   ...cacheActions,
+  ...miscActions,
 }
 
 export default UIActions
