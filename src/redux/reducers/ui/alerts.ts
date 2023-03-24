2160091834b18cbb6e978cbabbc99137b6841551
kruzhambus <maks.mrug.ru@gmail.com>
2023-03-24 22:15:41 +0200

    update eslint config, migrate ui/alerts

--- src/redux/reducers/ui/alerts.ts
+++ src/redux/reducers/ui/alerts.ts
@@ -1,13 +1,37 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-
-// alerts: [],
-// total: 0,
-// pageTotal: 0,
-// loading: false,
+import { IAlerts } from 'redux/models/IAlerts'
 
 interface IInitialState {
-    alerts: any[]
+    alerts: IAlerts[]
     total: number
     pageTotal: number
     loading: boolean
 }
+
+const initialState: IInitialState = {
+  alerts: [],
+  total: 0,
+  pageTotal: 0,
+  loading: false,
+}
+
+const alertsSlice = createSlice({
+  name: 'alerts',
+  initialState,
+  reducers: {
+    setProjectAlerts(state, action: PayloadAction<IAlerts[]>) {
+      state.alerts = action.payload
+    },
+    setProjectAlertsTotal(state, { payload }: PayloadAction<{ total: number, pageTotal: number }>) {
+      state.total = payload.total
+      state.pageTotal = payload.pageTotal
+    },
+    setProjectAlertsLoading(state, { payload }: PayloadAction<boolean>) {
+      state.loading = payload
+    },
+  },
+})
+
+export const alertsActions = alertsSlice.actions
+
+export default alertsSlice.reducer
