3906b756cd741cb8862eb614ecf604d15e7c3179
kruzhambus <maks.mrug.ru@gmail.com>
2023-04-13 00:34:59 +0300

    update checkbox test

--- src/ui/test/Checkbox.test.tsx
+++ src/ui/test/Checkbox.test.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Checkbox from '../Checkbox'
 
 describe('Checkbox', () => {
-  // do not use getByRole
   test('renders a checkbox', () => {
     const { getByText } = render(<Checkbox label='Checkbox' checked />)
     expect(getByText('Checkbox')).toBeInTheDocument()
@@ -16,5 +15,14 @@ describe('Checkbox', () => {
       <Checkbox id='custom-id' label='Checkbox' checked />,
     )
     expect(getByRole('checkbox')).toHaveAttribute('id', 'custom-id')
+  })
+
+  test('clicking the checkbox calls the onChange handler', () => {
+    const onChange = jest.fn()
+    const { getByRole } = render(
+      <Checkbox label='Checkbox' checked onChange={onChange} />,
+    )
+    fireEvent.click(getByRole('checkbox'))
+    expect(onChange).toHaveBeenCalled()
   })
 })
