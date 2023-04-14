3d8980f6fe0ac5e64486141723a8c2b10192db7a
kruzhambus <maks.mrug.ru@gmail.com>
2023-04-15 00:13:57 +0300

    add: Flatpicker tests and Alerts test fix

--- src/ui/test/Alert.test.tsx
+++ src/ui/test/Alert.test.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import AlertTemplate from '../Alert'
+import '@testing-library/jest-dom'
 
 describe('AlertTemplate', () => {
   const closeMock = jest.fn()
