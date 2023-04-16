e91f471e45c9e6f7bcc4c80274002e4a0740944b
Blue Mouse <durexp@protonmail.ch>
2023-04-16 04:23:27 +0100

    rm clg

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -578,8 +578,6 @@ export class AuthService {
   ) {
     let tokenInfo
 
-    console.log(token, hash)
-
     try {
       tokenInfo = await this.oauth2Client.getTokenInfo(token)
     } catch (reason) {
@@ -588,8 +586,6 @@ export class AuthService {
     }
 
     const { sub, email } = tokenInfo
-
-    console.log(sub, email)
 
     // TODO: Only store the email if email_verified is true
     const dataToStore = JSON.stringify({ sub, email })
