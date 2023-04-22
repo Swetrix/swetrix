e497306020ec118e0a6e5e3c7f11bc2ea2158457
kruzhambus <maks.mrug.ru@gmail.com>
2023-04-22 22:44:48 +0300

    render flatpicker with custom options

--- src/pages/Project/Settings/ProjectSettings.tsx
+++ src/pages/Project/Settings/ProjectSettings.tsx
@@ -25,7 +25,7 @@ import { IProject } from 'redux/models/IProject'
 import { IUser } from 'redux/models/IUser'
 import { IProjectForShared, ISharedProject } from 'redux/models/ISharedProject'
 import {
-  createProject, updateProject, deleteProject, resetProject, transferProject,
+  createProject, updateProject, deleteProject, resetProject, transferProject, deletePartially,
 } from 'api'
 import Input from 'ui/Input'
 import Button from 'ui/Button'
