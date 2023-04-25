3a10a7ef1168a25e1419a3e3934b34c6bc110d61
Blue Mouse <durexp@protonmail.ch>
2023-04-25 22:34:51 +0100

    added selfhosted tables initialisation script

---
+++ migrations/clickhouse/initialise_selfhosted.js
@@ -0,0 +1,29 @@
+// This script initialises the Swetrix cloud edition database and tables if they're absent
+const { queriesRunner, dbName } = require('./setup')
+const { initialiseDatabase } = require('./initialise_database')
+
+const CLICKHOUSE_INIT_QUERIES = [
+  `CREATE TABLE IF NOT EXISTS ${dbName}.project
+  (
+    id FixedString(12),
+    name String,
+    origins String,
+    active Int8,
+    public Int8,
+    created DateTime
+  )
+  ENGINE = MergeTree()
+  PARTITION BY toYYYYMM(created)
+  ORDER BY (created);`,
+]
+
+const initialiseSelfhosted = async () => {
+  try {
+    await initialiseDatabase()
+    await queriesRunner(CLICKHOUSE_INIT_QUERIES)
+  } catch (reason) {
+    console.error(`[ERROR] Error occured whilst initialising the database: ${reason}`)
+  }
+}
+
+initialiseSelfhosted()
