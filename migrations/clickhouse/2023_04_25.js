566cdf2ee6ba06adece8f4df2a0f613b6924b813
Blue Mouse <durexp@protonmail.ch>
2023-04-25 17:02:31 +0100

    using LowCardinality for performance table

--- migrations/clickhouse/2023_04_25.js
+++ migrations/clickhouse/2023_04_25.js
@@ -58,6 +58,35 @@ const queries = [
 
   `DROP TABLE ${dbName}.customEV`,
   `RENAME TABLE ${dbName}.customEV_temp TO ${dbName}.customEV`,
+
+  // Performance -> LowCardinality
+  `DROP TABLE IF EXISTS ${dbName}.performance_temp`,
+  `CREATE TABLE IF NOT EXISTS ${dbName}.performance_temp
+  (
+    pid FixedString(12),
+    pg Nullable(String),
+    dv LowCardinality(Nullable(String)),
+    br LowCardinality(Nullable(String)),
+    cc Nullable(FixedString(2)),
+    dns Nullable(UInt32),
+    tls Nullable(UInt32),
+    conn Nullable(UInt32),
+    response Nullable(UInt32),
+    render Nullable(UInt32),
+    domLoad Nullable(UInt32),
+    pageLoad Nullable(UInt32),
+    ttfb Nullable(UInt32),
+    created DateTime
+  )
+  ENGINE = MergeTree()
+  PARTITION BY toYYYYMM(created)
+  ORDER BY (pid, created);`,
+
+  `INSERT INTO ${dbName}.performance_temp (pid, pg, dv, br, cc, dns, tls, conn, response, render, domLoad, pageLoad, ttfb, created)
+  SELECT pid, pg, dv, br, cc, dns, tls, conn, response, render, domLoad, pageLoad, ttfb, created FROM ${dbName}.performance`,
+
+  `DROP TABLE ${dbName}.performance`,
+  `RENAME TABLE ${dbName}.performance_temp TO ${dbName}.performance`,
 ]
 
 queriesRunner(queries)
