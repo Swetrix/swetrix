e5c294afb5fc82f88537c338f45ab91662ae7d98
Blue Mouse <durexp@protonmail.ch>
2023-04-25 17:25:57 +0100

    disable query logging

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -41,6 +41,7 @@ const clickhouse = new ClickHouse({
     output_format_json_quote_64bit_integers: 0,
     enable_http_compression: 0,
     database: CLICKHOUSE_DATABASE,
+    log_queries: 0,
   },
 })
 
