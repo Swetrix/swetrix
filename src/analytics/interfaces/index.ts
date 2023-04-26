7f452ed2e5242aa62ce491891de1a37c373b4776
Blue Mouse <durexp@protonmail.ch>
2023-04-27 00:06:39 +0100

    fix linting issues

--- src/analytics/interfaces/index.ts
+++ src/analytics/interfaces/index.ts
@@ -45,7 +45,7 @@ export interface IGenerateXAxis {
 }
 
 export interface IExtractChartData {
-  visits: number[],
-  uniques: number[],
-  sdur: number[],
+  visits: number[]
+  uniques: number[]
+  sdur: number[]
 }
