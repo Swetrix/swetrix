e5dc3866616f1843454a2b8ba72b4b4b843afa06
Yevhenii Kulisidi <109991141+yevheniikulisidi@users.noreply.github.com>
2023-04-28 23:21:52 +0300

    NestJS Telegraf module was fine-tuned

--- src/integrations/telegram/telegram.module.ts
+++ src/integrations/telegram/telegram.module.ts
@@ -1,4 +1,31 @@
 import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { TelegrafModule } from 'nestjs-telegraf'
 
-@Module({})
+@Module({
+  imports: [
+    TelegrafModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        token: configService.get<string>('TELEGRAM_BOT_TOKEN'),
+        launchOptions: {
+          dropPendingUpdates: false,
+          ...(process.env.NODE_ENV === 'production' && {
+            webhook: {
+              domain: configService.get<string>('TELEGRAM_WEBHOOK_DOMAIN'),
+              hookPath: configService.get<string>('TELEGRAM_WEBHOOK_PATH'),
+              ipAddress: configService.get<string>(
+                'TELEGRAM_WEBHOOK_IP_ADDRESS',
+              ),
+              maxConnections: 100,
+              secretToken: configService.get<string>(
+                'TELEGRAM_WEBHOOK_SECRET_TOKEN',
+              ),
+            },
+          }),
+        },
+      }),
+    }),
+  ],
+})
 export class TelegramModule {}
