990c4eacf1eeb85c1e26f7d8d487f3cbcf8bab73
Blue Mouse <durexp@protonmail.ch>
2023-04-29 03:50:14 +0100

    removed beta from IP's to blacklist

--- src/pages/Project/Settings/ProjectSettings.tsx
+++ src/pages/Project/Settings/ProjectSettings.tsx
@@ -420,7 +420,6 @@ const ProjectSettings = ({
                 className='mt-4'
                 onChange={handleInput}
                 error={beenSubmitted ? errors.ipBlacklist : null}
-                isBeta
               />
               <Checkbox
                 checked={Boolean(form.active)}
