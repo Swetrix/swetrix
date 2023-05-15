5fed9ef1cdcd2f629ec897a41211a6606e9479e1
Blue Mouse <durexp@protonmail.ch>
2023-05-15 22:29:45 +0100

    fixed login

--- apps/selfhosted/src/auth/dtos/login.dto.ts
+++ apps/selfhosted/src/auth/dtos/login.dto.ts
@@ -1,8 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger'
+import {
+  IsEmail,
+  MinLength,
+} from 'class-validator'
 
 export class LoginRequestDto {
-  email: string
-  password: string
+  @ApiProperty({
+    description: 'User email',
+    example: 'yourusername@example.com',
+    maxLength: 254,
+    minLength: 6,
+  })
+  @IsEmail({}, { message: 'Please enter the valid email.' })
+  public readonly email: string
+
+  @ApiProperty({
+    description: 'User password',
+    example: '%d7*c4W45p',
+    minLength: 8,
+  })
+  @MinLength(8, { message: 'Min length is $constraint1 characters' })
+  public readonly password: string
 }
 
 export class LoginResponseDto {
