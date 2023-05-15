4bc53fea6f8dcfe96ce9d094729f44875c259210
Blue Mouse <durexp@protonmail.ch>
2023-05-15 17:48:27 +0100

    fixed missing AppLoggerModule

--- apps/selfhosted/src/user/user.module.ts
+++ apps/selfhosted/src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 
 import { UserController } from './user.controller'
 import { UserService } from './user.service'
+import { AppLoggerModule } from '../logger/logger.module'
 
 @Module({
-  imports: [],
+  imports: [AppLoggerModule],
   providers: [UserService],
   exports: [UserService],
   controllers: [UserController],
