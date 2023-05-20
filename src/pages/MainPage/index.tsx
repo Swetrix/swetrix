f10010df9ede8c485b4aa523d65056b2258ee51f
Blue Mouse <durexp@protonmail.ch>
2023-05-20 20:14:06 +0100

    added 1 digit after the separator for the stats

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -148,9 +148,9 @@ const Main = (): JSX.Element => {
   const { authenticated } = useSelector((state: StateType) => state.auth)
   const { stats, lastBlogPost } = useSelector((state: StateType) => state.ui.misc)
 
-  const events = nFormatterSeparated(Number(stats.events), 0)
-  const users = nFormatterSeparated(Number(stats.users), 0)
-  const websites = nFormatterSeparated(Number(stats.projects), 0)
+  const events = nFormatterSeparated(Number(stats.events))
+  const users = nFormatterSeparated(Number(stats.users))
+  const websites = nFormatterSeparated(Number(stats.projects))
 
   return (
     <Title title={t('titles.main')}>
