e946ef4c6d226b70acf825b42d1a33efd3a9e65c
Blue Mouse <durexp@protonmail.ch>
2023-05-22 03:27:49 +0100

    added planCode != 'trial' clause

--- migrations/mysql/2023_05_22.sql
+++ migrations/mysql/2023_05_22.sql
@@ -1,2 +1,2 @@
 alter table user add column `tierCurrency` varchar(3) DEFAULT NULL AFTER cancellationEffectiveDate;
-update user set tierCurrency='USD' where planCode != 'none' and planCode != 'free';
+update user set tierCurrency='USD' where planCode != 'none' and planCode != 'free' and planCode != 'trial';
