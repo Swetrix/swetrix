ec810168c850e650058741fd70b1c6a0cd7134c2
Blue Mouse <durexp@protonmail.ch>
2023-05-22 02:24:11 +0100

    set tierCurrency to USD for all users who are paying for service

--- migrations/mysql/2023_05_22.sql
+++ migrations/mysql/2023_05_22.sql
@@ -1,1 +1,2 @@
 alter table user add column `tierCurrency` varchar(3) DEFAULT NULL AFTER cancellationEffectiveDate;
+update user set tierCurrency='USD' where planCode != 'none' and planCode != 'free';
