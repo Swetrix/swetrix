4a12543625e82fd2c5142164fc2efae414387f4b
Yevhenii Kulisidi <109991141+yevheniikulisidi@users.noreply.github.com>
2023-05-25 22:55:18 +0300

    Fixed an error with importing "Project" module

--- src/projects-exports/projects-exports.module.ts
+++ src/projects-exports/projects-exports.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { ProjectModule } from 'src/project/project.module'
 import { ProjectExport } from './entity/project-export.entity'
 import { ProjectsExportsController } from './projects-exports.controller'
 import { ProjectsExportsService } from './projects-exports.service'
 import { ProjectExportRepository } from './repository/project-export.repository'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProjectExport])],
+  imports: [TypeOrmModule.forFeature([ProjectExport]), ProjectModule],
   controllers: [ProjectsExportsController],
   providers: [ProjectsExportsService, ProjectExportRepository],
 })
