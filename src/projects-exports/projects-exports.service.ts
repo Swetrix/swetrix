193c57556661e465debfebd4078898b13d116c77
Yevhenii Kulisidi <109991141+yevheniikulisidi@users.noreply.github.com>
2023-05-26 22:59:18 +0300

    Added saving project export to database

--- src/projects-exports/projects-exports.service.ts
+++ src/projects-exports/projects-exports.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@nestjs/common'
 import { CreateExportDto } from './dto/create-export.dto'
+import { ProjectExport } from './entity/project-export.entity'
+import { ProjectExportRepository } from './repository/project-export.repository'
 
 @Injectable()
 export class ProjectsExportsService {
+  constructor(
+    private readonly projectExportRepository: ProjectExportRepository,
+  ) {}
+
   async createProjectExport(
     projectId: string,
-    createExportDto: CreateExportDto,
-  ): Promise<unknown> {
-    return {}
+    { startDate, endDate }: CreateExportDto,
+  ): Promise<ProjectExport> {
+    const projectExport =
+      await this.projectExportRepository.createProjectExport(projectId, {
+        startDate,
+        endDate,
+      })
+
+    return projectExport
   }
 }
