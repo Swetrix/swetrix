d465672c889b544b2bb873fcb891d5a2c9f09c44
Blue Mouse <durexp@protonmail.ch>
2023-05-29 22:57:52 +0100

    passwordHash after isTransferring

--- migrations/mysql/2023_05_18.sql
+++ migrations/mysql/2023_05_18.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `project` ADD COLUMN `passwordHash` VARCHAR(60) DEFAULT NULL AFTER `public`;
+ALTER TABLE `project` ADD COLUMN `passwordHash` VARCHAR(60) DEFAULT NULL AFTER `isTransferring`;
 ALTER TABLE `project` ADD COLUMN `isPasswordProtected` TINYINT(1) NOT NULL DEFAULT 0 AFTER `passwordHash`;
