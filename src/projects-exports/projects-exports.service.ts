6ca7c786eca5bc00f7f7b46a998a7f5dfaea283c
Yevhenii Kulisidi <109991141+yevheniikulisidi@users.noreply.github.com>
2023-05-29 20:07:09 +0300

    Added conversion and archiving functions

--- src/projects-exports/projects-exports.service.ts
+++ src/projects-exports/projects-exports.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common'
+import AdmZip from 'adm-zip'
+import { writeFile } from 'fs/promises'
+import { parseAsync } from 'json2csv'
+import { tmpdir } from 'os'
+import { join } from 'path'
 import { CreateExportDto } from './dto/create-export.dto'
 import { ProjectExport } from './entity/project-export.entity'
 import { ProjectExportRepository } from './repository/project-export.repository'
@@ -20,5 +25,20 @@ export class ProjectsExportsService {
       })
 
     return projectExport
+  }
+
+  async convertJSONtoCSV(
+    jsonData: object[],
+    exportId: string,
+    fileName: 'analytics' | 'captcha' | 'custom-events' | 'performance',
+  ): Promise<void> {
+    const csvData = await parseAsync(jsonData)
+    await writeFile(join(tmpdir(), exportId, `${fileName}.csv`), csvData)
+  }
+
+  async createZipArchive(exportId: string): Promise<void> {
+    const zip = new AdmZip()
+    zip.addLocalFolder(join(tmpdir(), exportId))
+    zip.writeZip(join(tmpdir(), `${exportId}.zip`))
   }
 }
