a84bc10ebfd18cf33aeaca0b931ea668db51dd39
Yevhenii Kulisidi <109991141+yevheniikulisidi@users.noreply.github.com>
2023-05-30 23:43:49 +0300

    Added function to get project data

---
+++ src/projects-exports/interface/project-data.interface.ts
@@ -0,0 +1,6 @@
+export interface ProjectData {
+  analytics: object[]
+  captcha: object[]
+  customEvents: object[]
+  performance: object[]
+}
--- src/projects-exports/projects-exports.service.ts
+++ src/projects-exports/projects-exports.service.ts
@@ -4,8 +4,10 @@ import { writeFile } from 'fs/promises'
 import { parseAsync } from 'json2csv'
 import { tmpdir } from 'os'
 import { join } from 'path'
+import { clickhouse } from 'src/common/constants'
 import { CreateExportDto } from './dto/create-export.dto'
 import { ProjectExport } from './entity/project-export.entity'
+import { ProjectData } from './interface/project-data.interface'
 import { ProjectExportRepository } from './repository/project-export.repository'
 
 @Injectable()
@@ -25,6 +27,50 @@ export class ProjectsExportsService {
       })
 
     return projectExport
+  }
+
+  async getProjectData(
+    projectId: string,
+    startDate: Date,
+    endDate: Date,
+  ): Promise<ProjectData> {
+    const formattedStartDate = `${new Date(startDate)
+      .toISOString()
+      .slice(0, 10)} 00:00:00`
+    const formattedEndDate = `${new Date(endDate)
+      .toISOString()
+      .slice(0, 10)} 23:59:59`
+
+    const query = `
+      SELECT *
+      FROM {table}
+      WHERE pid = {pid: FixedString(12)}
+        AND created >= {startDate: DateTime}
+        AND created <= {endDate: DateTime}
+    `
+
+    const params = {
+      pid: projectId,
+      startDate: formattedStartDate,
+      endDate: formattedEndDate,
+    }
+
+    const queries: { table: string; resultKey: keyof ProjectData }[] = [
+      { table: 'analytics', resultKey: 'analytics' },
+      { table: 'captcha', resultKey: 'captcha' },
+      { table: 'customEV', resultKey: 'customEvents' },
+      { table: 'performance', resultKey: 'performance' },
+    ]
+
+    const resultPromises = queries.map(async ({ table, resultKey }) => {
+      const result = await clickhouse
+        .query(query.replace('{table}', table), { params })
+        .toPromise()
+      return { [resultKey]: result } as Partial<ProjectData>
+    })
+
+    const results = await Promise.all(resultPromises)
+    return Object.assign({}, ...results) as ProjectData
   }
 
   async convertJSONtoCSV(
