98c4b2904b17271ce48256d88890e160c9b473d9
Maksim Mrug <80621719+kruzhambus@users.noreply.github.com>
2023-06-03 20:26:42 +0300

    tailwind connect

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,7 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
+import tailwindCss from "./css/tailwind.css";
+
 import {
   Links,
   LiveReload,
@@ -10,7 +12,10 @@ import {
 } from "@remix-run/react";
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  ...(cssBundleHref ? [
+    { rel: "stylesheet", href: cssBundleHref },
+    { rel: "stylesheet", href: tailwindCss },
+  ] : []),
 ];
 
 export default function App() {
