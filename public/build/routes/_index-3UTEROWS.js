86af56be512b4dc00735a5bd080053d435febfcd
Maksim Mrug <80621719+kruzhambus@users.noreply.github.com>
2023-06-05 18:52:19 +0300

    update gitignore

---
+++ public/build/routes/_index-3UTEROWS.js
@@ -0,0 +1,105 @@
+import {
+  __toESM,
+  init_buffer,
+  init_dirname,
+  init_filename,
+  init_global,
+  init_process,
+  require_jsx_dev_runtime
+} from "/build/_shared/chunk-NLQNPAAV.js";
+
+// browser-route-module:routes/_index.tsx?browser
+init_global();
+init_dirname();
+init_filename();
+init_buffer();
+init_process();
+
+// app/routes/_index.tsx
+init_global();
+init_dirname();
+init_filename();
+init_buffer();
+init_process();
+var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime());
+var meta = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" }
+  ];
+};
+function Index() {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { style: { fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }, children: [
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("h1", { children: "Welcome to Remix" }, void 0, false, {
+      fileName: "app/routes/_index.tsx",
+      lineNumber: 13,
+      columnNumber: 7
+    }, this),
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("ul", { children: [
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("li", { children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(
+        "a",
+        {
+          target: "_blank",
+          href: "https://remix.run/tutorials/blog",
+          rel: "noreferrer",
+          children: "15m Quickstart Blog Tutorial"
+        },
+        void 0,
+        false,
+        {
+          fileName: "app/routes/_index.tsx",
+          lineNumber: 16,
+          columnNumber: 11
+        },
+        this
+      ) }, void 0, false, {
+        fileName: "app/routes/_index.tsx",
+        lineNumber: 15,
+        columnNumber: 9
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("li", { children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(
+        "a",
+        {
+          target: "_blank",
+          href: "https://remix.run/tutorials/jokes",
+          rel: "noreferrer",
+          children: "Deep Dive Jokes App Tutorial"
+        },
+        void 0,
+        false,
+        {
+          fileName: "app/routes/_index.tsx",
+          lineNumber: 25,
+          columnNumber: 11
+        },
+        this
+      ) }, void 0, false, {
+        fileName: "app/routes/_index.tsx",
+        lineNumber: 24,
+        columnNumber: 9
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("li", { children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", { target: "_blank", href: "https://remix.run/docs", rel: "noreferrer", children: "Remix Docs" }, void 0, false, {
+        fileName: "app/routes/_index.tsx",
+        lineNumber: 34,
+        columnNumber: 11
+      }, this) }, void 0, false, {
+        fileName: "app/routes/_index.tsx",
+        lineNumber: 33,
+        columnNumber: 9
+      }, this)
+    ] }, void 0, true, {
+      fileName: "app/routes/_index.tsx",
+      lineNumber: 14,
+      columnNumber: 7
+    }, this)
+  ] }, void 0, true, {
+    fileName: "app/routes/_index.tsx",
+    lineNumber: 12,
+    columnNumber: 5
+  }, this);
+}
+export {
+  Index as default,
+  meta
+};
+//# sourceMappingURL=/build/routes/_index-3UTEROWS.js.map
