be9a5016ea429a534c0580143fed6ac3579c77c4
Maksim Mrug <80621719+kruzhambus@users.noreply.github.com>
2023-06-13 23:21:29 +0300

    add rourting for resetPassword

---
+++ app/routes/password-reset.tsx
@@ -0,0 +1,20 @@
+import ForgotPassword from 'pages/Auth/ForgotPassword'
+import type { V2_MetaFunction } from '@remix-run/node'
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: 'Reset password' },
+    {
+      name: 'description',
+      content: 'Reset your password',
+    },
+    {
+      name: 'keywords',
+      content: 'reset, password, forgot, forgot password',
+    },
+  ]
+}
+
+export default function Index() {
+  return <ForgotPassword />
+}
