32b9231011a31ccb589399dd6c2f4a9338464bcf
Maksim Mrug <80621719+kruzhambus@users.noreply.github.com>
2023-06-13 18:05:01 +0300

    fix nano id

--- app/utils/random.ts
+++ app/utils/random.ts
@@ -1,5 +1,10 @@
-import { customAlphabet } from 'nanoid'
-
 const legalCharacters = '1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'
 
-export const nanoid = customAlphabet(legalCharacters, 12)
+const loadNanoid = async () => {
+  const nanoidModule = await import('nanoid')
+  const nanoid = nanoidModule.customAlphabet(legalCharacters, 12)
+
+  return nanoid
+}
+
+export const nanoid = loadNanoid()
