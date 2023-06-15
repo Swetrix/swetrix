4b68244e8ff5a26a079b0b307eb797cbf67ed871
Blue Mouse <durexp@protonmail.ch>
2023-06-15 01:38:15 +0100

    use updated config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -50,9 +50,9 @@ export default {
     },
     minHeight: {
       72: '18rem',
-      page: 'calc(100vh - 74px)', // header height -> 74px
-      'min-footer': 'calc(100vh - 160px)', // 74px + 88px footer height
-      'min-footer-ad': 'calc(100vh - 265px)', // 74px + 88px footer height
+     page: 'calc(100vh - 74px)', // header height -> 74px
+     'min-footer': 'calc(100vh - 148px)',
+     'min-footer-ad': 'calc(100vh - 265px)',
     },
   },
   variants: {
