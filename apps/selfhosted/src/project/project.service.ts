fa61d909bd76eb3a6679bd77485d7d0a4303d816
Blue Mouse <durexp@protonmail.ch>
2023-06-18 00:04:47 +0100

    fixed an issue with project cration

--- apps/selfhosted/src/project/project.service.ts
+++ apps/selfhosted/src/project/project.service.ts
@@ -164,11 +164,11 @@ export class ProjectService {
     return updProject
   }
 
-  validateProject(projectDTO: ProjectDTO | CreateProjectDTO) {
+  validateProject(projectDTO: ProjectDTO, creatingProject = false) {
     if (_size(projectDTO.name) > 50)
       throw new UnprocessableEntityException('The project name is too long')
 
-    if (projectDTO instanceof CreateProjectDTO) {
+    if (creatingProject) {
       return
     }
 
