fae6203ba70e57dc428f5a8535979cfeb99ba96a
Maksim Mrug <80621719+kruzhambus@users.noreply.github.com>
2023-06-22 19:26:53 +0300

    new multiselect

--- app/routes/projects.settings.$id.tsx
+++ app/routes/projects.settings.$id.tsx
@@ -1,4 +1,12 @@
 import ProjectSettings from 'pages/Project/Settings'
+import type { LinksFunction } from '@remix-run/node'
+import ProjectSettingsStyle from 'styles/ProjectSettings.css'
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: 'stylesheet', href: ProjectSettingsStyle },
+  ]
+}
 
 export default function Index() {
   return <ProjectSettings />
--- app/ui/MultiSelect.tsx
+++ app/ui/MultiSelect.tsx
@@ -1,28 +1,29 @@
 import React from 'react'
-import { MultiSelect } from 'react-multi-select-component'
+import Select from 'react-select'
 
 const Multiselect = ({ items, selected, onSelect }: {
   items: {
-    label: string,
-    value: string,
-    disabled?: boolean,
-    key?: string,
+    value: string | number,
+    label: string | number,
   }[],
   selected: {
-    label: string,
-    value: string,
+    value: string | number,
+    label: string | number,
   }[],
-  onSelect: (i: {
-    label: string,
-    value: string,
-  }[]) => void,
+  onSelect: (item: {
+    value: string | number,
+    label: string | number,
+  }) => void,
 }) => (
-  <MultiSelect
+  <Select
+    isMulti
+    isSearchable
+    // className='inline-flex w-full rounded-md border border-gray-300 shadow-sm px-3 md:px-4 py-2 bg-white text-sm font-medium text-gray-700 dark:text-gray-50 dark:border-gray-800 dark:bg-slate-800 hover:bg-gray-50 dark:hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500'
+    classNamePrefix='multiselect'
     options={items}
     value={selected}
+    // @ts-ignore
     onChange={onSelect}
-    labelledBy='Select'
-    isCreatable
   />
 )
 
