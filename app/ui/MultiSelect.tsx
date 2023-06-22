da41dc128eedb40fe38a651ce72b61d2e2f46f4b
Maksim Mrug <80621719+kruzhambus@users.noreply.github.com>
2023-06-22 23:04:33 +0300

    add dark theme

--- app/ui/MultiSelect.tsx
+++ app/ui/MultiSelect.tsx
@@ -24,10 +24,10 @@ const MultiSelect = ({
               setSelected(!selected)
             }}
           >
-            <div className='my-2 p-1 flex border border-gray-200 bg-white rounded '>
+            <div className='my-2 p-1 flex border rounded w-ful shadow-sm bg-white text-sm font-medium text-gray-700 dark:text-gray-50 dark:border-gray-800 dark:bg-slate-800 hover:bg-gray-50 dark:hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 '>
               <div className='flex flex-auto flex-wrap'>
                 {_map(label, (item) => (
-                  <div key={keyExtractor ? keyExtractor(item) : item} className='flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-indigo-700 bg-indigo-100 border border-indigo-300 '>
+                  <div key={keyExtractor ? keyExtractor(item) : item} className='flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-indigo-700 dark:text-gray-50 bg-indigo-100 dark:bg-slate-800 border border-indigo-300 dark:border-slate-500'>
                     <div className='text-xs font-normal leading-none max-w-full flex-initial'>{labelExtractor ? labelExtractor(item) : item}</div>
                     <div className='flex flex-auto flex-row-reverse'>
                       <div onClick={() => onRemove(item)}>
@@ -41,7 +41,7 @@ const MultiSelect = ({
                 ))}
               </div>
               <div className='text-gray-300 w-8 py-1 pl-2 pr-1 border-l flex items-center border-gray-200'>
-                <button type='button' onClick={() => setSelected(!selected)} className='cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none'>
+                <button type='button' onClick={() => setSelected(!selected)} className='cursor-pointer w-6 h-6 text-gray-600 dark:text-gray-300 outline-none focus:outline-none'>
                   <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%' fill='none' viewBox='0 0 24 24' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' className=' rotate-180 w-4 h-4'>
                     <polyline points='18 15 12 9 6 15' />
                   </svg>
@@ -50,11 +50,11 @@ const MultiSelect = ({
             </div>
           </div>
           {selected && (
-            <div className=' shadow top-100 bg-white z-40 w-full lef-0 rounded max-h-select overflow-y-auto max-h-[200px]'>
+            <div className=' shadow top-100 bg-white dark:bg-slate-800 z-40 w-full lef-0 rounded max-h-select overflow-y-auto max-h-[200px]'>
               <div className='flex flex-col w-full'>
                 {_map(items, (item) => (
-                  <div key={keyExtractor ? `${keyExtractor(item)}select` : `${item}select`} onClick={() => onSelect(item)} className='cursor-pointer w-full border-gray-100 rounded-t border-b hover:bg-indigo-100'>
-                    <div className='flex w-full items-center p-2 pl-2 border-transparent border-l-2 relative hover:border-indigo-100'>
+                  <div key={keyExtractor ? `${keyExtractor(item)}select` : `${item}select`} onClick={() => onSelect(item)} className='cursor-pointer w-full border-gray-100 dark:border-slate-500 rounded-t border-b hover:bg-indigo-100 dark:hover:bg-slate-700'>
+                    <div className='flex w-full items-center p-2 pl-2 border-transparent border-l-2 relative hover:border-indigo-100 dark:hover:border-slate-700'>
                       <div className='w-full items-center flex'>
                         <div className='mx-2 leading-6'>{labelExtractor ? labelExtractor(item) : item}</div>
                       </div>
@@ -67,7 +67,7 @@ const MultiSelect = ({
         </div>
       </div>
       {hint && (
-      <p className='mt-2 text-sm text-gray-500 dark:text-gray-300 whitespace-pre-line'>{hint}</p>
+        <p className='mt-2 text-sm text-gray-500 dark:text-gray-300 whitespace-pre-line'>{hint}</p>
       )}
     </div>
   )
