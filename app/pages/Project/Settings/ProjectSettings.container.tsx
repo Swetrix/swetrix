fb29bd6a009df3f629e877f73217cddf4a6eba18
Blue Mouse <durexp@protonmail.ch>
2023-06-23 01:11:13 +0100

    rm projectProtectedPassword

--- app/pages/Project/Settings/ProjectSettings.container.tsx
+++ app/pages/Project/Settings/ProjectSettings.container.tsx
@@ -16,7 +16,6 @@ const mapStateToProps = (state: StateType) => ({
   dashboardPaginationPageShared: state.ui.projects.dashboardPaginationPageShared,
   user: state.auth.user,
   isSharedProject: state.ui.projects.dashboardTabs === tabForSharedProject,
-  projectProtectedPassword: state.ui.projects.password,
 })
 
 const mapDispatchToProps = (dispatch: AppDispatch) => ({
