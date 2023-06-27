d1c5d4d44a93f3bb1d808fd89583b851f862a752
Blue Mouse <durexp@protonmail.ch>
2023-06-27 02:15:06 +0100

    added attribution to DB-IP.com

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -169,7 +169,7 @@ const Footer = ({ minimal, authenticated }: {
     return (
       <footer className='bg-gray-50 dark:bg-slate-900 border-t border-gray-200 dark:border-slate-800/50'>
         <div className='max-w-7xl mx-auto py-8 px-4 overflow-hidden sm:px-6 lg:px-8'>
-          <nav className='-mx-5 -my-2 flex flex-wrap justify-center' aria-label='Footer'>
+          <nav className='-mx-5 -my-2 flex flex-wrap justify-center relative' aria-label='Footer'>
             <div className='px-5 py-2'>
               <Link to={routesPath.contact} className='leading-6 text-slate-900 dark:text-gray-300 hover:text-slate-700 dark:hover:text-white'>
                 {t('footer.contact')}
@@ -195,6 +195,15 @@ const Footer = ({ minimal, authenticated }: {
                 {t('footer.status')}
               </a>
             </div>
+            <a
+              className='absolute -bottom-5 leading-6 text-slate-900 dark:text-gray-300 hover:text-slate-700 dark:hover:text-white'
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://db-ip.com'
+              aria-label='IP Geolocation by DB-IP (opens in a new tab)'
+            >
+              IP Geolocation by DB-IP
+            </a>
           </nav>
         </div>
       </footer>
