e3e999f24106667a6f89862fd601beedffa3a13d
Blue Mouse <durexp@protonmail.ch>
2023-07-22 01:58:06 +0100

    display IP-geolocation disclaimer on the project view page only

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,7 +1,9 @@
 import React, { memo } from 'react'
 import { Link } from '@remix-run/react'
 import { useTranslation } from 'react-i18next'
+import { useLocation } from '@remix-run/react'
 import _map from 'lodash/map'
+import _startsWith from 'lodash/startsWith'
 import Flag from 'react-flagkit'
 import PropTypes from 'prop-types'
 
@@ -160,6 +162,7 @@ const Footer = ({ minimal, authenticated }: {
 }): JSX.Element => {
   const { t, i18n: { language } } = useTranslation('common')
   const year = new Date().getFullYear()
+  const location = useLocation()
 
   if (isSelfhosted) {
     return <SelfHostedFooter />
@@ -195,15 +198,17 @@ const Footer = ({ minimal, authenticated }: {
                 {t('footer.status')}
               </a>
             </div>
-            <a
-              className='absolute -bottom-5 leading-6 text-slate-900 dark:text-gray-300 hover:text-slate-700 dark:hover:text-white'
-              target='_blank'
-              rel='noopener noreferrer'
-              href='https://db-ip.com'
-              aria-label='IP Geolocation by DB-IP (opens in a new tab)'
-            >
-              IP Geolocation by DB-IP
-            </a>
+            {_startsWith(location.pathname, '/projects/') && (
+              <a
+                className='absolute -bottom-5 leading-6 text-slate-900 dark:text-gray-300 hover:text-slate-700 dark:hover:text-white'
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://db-ip.com'
+                aria-label='IP Geolocation by DB-IP (opens in a new tab)'
+              >
+                IP Geolocation by DB-IP
+              </a>
+            )}
           </nav>
         </div>
       </footer>
