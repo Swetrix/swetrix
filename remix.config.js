4eb189a997208cfc49205cf83f48c13a005e5013
Blue Mouse <durexp@protonmail.ch>
2023-09-08 17:37:26 +0100

    fixed route convention warning

--- remix.config.js
+++ remix.config.js
@@ -1,3 +1,7 @@
+const {
+  createRoutesFromFolders,
+} = require("@remix-run/v1-route-convention")
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
@@ -14,7 +18,7 @@ module.exports = {
     v2_normalizeFormMethod: true,
     v2_dev: true,
     v2_headers: true,
-    // v2_routeConvention: true,
+    v2_routeConvention: true,
   },
   serverDependenciesToBundle: [
     'axios',
@@ -28,4 +32,8 @@ module.exports = {
     'robust-predicates',
   ],
   serverMinify: process.env.NODE_ENV === 'production',
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes)
+  }
 }
