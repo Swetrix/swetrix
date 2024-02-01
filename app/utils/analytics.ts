9e1819b486cd304458e4fcd51671fce243bef4a8
Blue Mouse <durexp@protonmail.ch>
2024-02-01 21:33:29 +0000

    upgraded swetrix.js to v3

--- app/utils/analytics.ts
+++ app/utils/analytics.ts
@@ -3,21 +3,54 @@ import { isSelfhosted } from 'redux/constants'
 
 const SWETRIX_PID = 'STEzHcB1rALV'
 
-Swetrix.init(SWETRIX_PID)
+const checkIgnore = (path: string | undefined | null, ignore: RegExp[]) => {
+  if (!path) {
+    return false
+  }
+
+  for (let i = 0; i < ignore.length; ++i) {
+    if (ignore[i].test(path)) {
+      return true
+    }
+  }
+
+  return false
+}
+
+const pathsToIgnore = [
+  /^\/projects\/(?!new$)[^/]+$/i,
+  /^\/projects\/settings/i,
+  /^\/verify/i,
+  /^\/password-reset/i,
+  /^\/change-email/i,
+  /^\/share/i,
+  /^\/captchas\/(?!new$)[^/]+$/i,
+  /^\/captchas\/settings/i,
+]
+
+Swetrix.init(SWETRIX_PID, {
+  devMode: true,
+})
 
 const trackViews = () => {
   if (!isSelfhosted) {
     Swetrix.trackViews({
-      ignore: [
-        /^\/projects\/(?!new$)[^/]+$/i,
-        /^\/projects\/settings/i,
-        /^\/verify/i,
-        /^\/password-reset/i,
-        /^\/change-email/i,
-        /^\/share/i,
-        /^\/captchas\/(?!new$)[^/]+$/i,
-        /^\/captchas\/settings/i,
-      ],
+      callback: ({ pg, prev }) => {
+        const result = {
+          pg,
+          prev,
+        }
+
+        if (checkIgnore(pg, pathsToIgnore)) {
+          result.pg = null
+        }
+
+        if (checkIgnore(prev, pathsToIgnore)) {
+          result.prev = null
+        }
+
+        return result
+      },
       heartbeatOnBackground: true,
     })
   }
