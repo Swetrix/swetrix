1dacb232a600c0dcd21fbbdf0221041de5faf460
Blue Mouse <durexp@protonmail.ch>
2024-03-30 23:17:49 +0000

    added error-tracking related table

---
+++ migrations/clickhouse/2024_03_30.js
@@ -0,0 +1,28 @@
+const { queriesRunner, dbName } = require('./setup')
+
+const queries = [
+  `CREATE TABLE IF NOT EXISTS ${dbName}.errors
+  (
+    psid Nullable(UInt64),
+    pid FixedString(12),
+    pg Nullable(String),
+    dv LowCardinality(Nullable(String)),
+    br LowCardinality(Nullable(String)),
+    os LowCardinality(Nullable(String)),
+    lc LowCardinality(Nullable(String)),
+    cc LowCardinality(Nullable(FixedString(2))),
+    rg LowCardinality(Nullable(String)),
+    ct Nullable(String),
+    name Nullable(String),
+    message Nullable(String),
+    lineno Nullable(UInt32),
+    colno Nullable(UInt32),
+    filename Nullable(String),
+    created DateTime('UTC')
+  )
+  ENGINE = MergeTree()
+  PARTITION BY toYYYYMM(created)
+  ORDER BY (pid, created);`,
+]
+
+queriesRunner(queries)
--- migrations/clickhouse/initialise_database.js
+++ migrations/clickhouse/initialise_database.js
@@ -86,6 +86,30 @@ const CLICKHOUSE_INIT_QUERIES = [
   PARTITION BY toYYYYMM(created)
   ORDER BY (pid, created);`,
 
+  // Error events table
+  `CREATE TABLE IF NOT EXISTS ${dbName}.errors
+  (
+    psid Nullable(UInt64),
+    pid FixedString(12),
+    pg Nullable(String),
+    dv LowCardinality(Nullable(String)),
+    br LowCardinality(Nullable(String)),
+    os LowCardinality(Nullable(String)),
+    lc LowCardinality(Nullable(String)),
+    cc LowCardinality(Nullable(FixedString(2))),
+    rg LowCardinality(Nullable(String)),
+    ct Nullable(String),
+    name Nullable(String),
+    message Nullable(String),
+    lineno Nullable(UInt32),
+    colno Nullable(UInt32),
+    filename Nullable(String),
+    created DateTime('UTC')
+  )
+  ENGINE = MergeTree()
+  PARTITION BY toYYYYMM(created)
+  ORDER BY (pid, created);`,
+
   // The CAPTCHA data table
   `CREATE TABLE IF NOT EXISTS ${dbName}.captcha
   (
