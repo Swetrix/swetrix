16dfec0900795c7d4fd79527be29d051443507d9
Yevhenii Kulisidi <109991141+yevheniikulisidi@users.noreply.github.com>
2024-04-06 14:56:24 +0000

    Added caching of Open Graph images

--- apps/production/src/og-image/og-image.controller.ts
+++ apps/production/src/og-image/og-image.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Query, Get, Header, Res } from '@nestjs/common'
 import { ApiTags, ApiQuery, ApiResponse } from '@nestjs/swagger'
 import { Response } from 'express'
+import { createHash } from 'crypto'
 
 import { OgImageService } from './og-image.service'
 import { AppLoggerService } from '../logger/logger.service'
+import { redis } from '../common/constants'
 
 @ApiTags('Opengraph Image Generator')
 @Controller(['v1/og-image'])
@@ -26,11 +28,24 @@ export class OgImageController {
   async getShared(
     @Query('title') title: string,
     @Res() res: Response,
-  ): Promise<any> {
+  ): Promise<void> {
     this.logger.log({ title }, 'GET /og-image')
 
-    const image = await this.ogImageService.getOgImage(title)
+    const titleHash = createHash('sha256').update(title).digest('hex')
 
-    res.end(image)
+    let cachedImage = await redis.getBuffer(`og_images:${titleHash}`)
+
+    if (!cachedImage) {
+      const image = await this.ogImageService.getOgImage(title)
+      await redis.set(
+        `og_images:${titleHash}`,
+        image,
+        'PX',
+        14 * 24 * 60 * 60 * 1000,
+      )
+      cachedImage = image
+    }
+
+    res.end(cachedImage)
   }
 }
