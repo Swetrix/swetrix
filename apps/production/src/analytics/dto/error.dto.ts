0f9beb3cc30a99f3eba5a0cda6692096e4b9862a
Blue Mouse <durexp@protonmail.ch>
2024-05-05 16:28:40 +0100

    validation

--- apps/production/src/analytics/dto/error.dto.ts
+++ apps/production/src/analytics/dto/error.dto.ts
@@ -1,5 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty } from 'class-validator'
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Matches,
+} from 'class-validator'
+import { PID_REGEX } from '../../common/constants'
 
 export class ErrorDTO {
   @ApiProperty({
@@ -8,6 +16,9 @@ export class ErrorDTO {
     description: 'The project ID',
   })
   @IsNotEmpty()
+  @Matches(PID_REGEX, {
+    message: 'Incorrect project ID format.',
+  })
   pid: string
 
   // Tracking metrics
@@ -15,18 +26,24 @@ export class ErrorDTO {
     example: 'Europe/Kiev',
     description: "User's timezone",
   })
+  @IsOptional()
+  @IsString()
   tz?: string
 
   @ApiProperty({
     example: '/articles/my-awesome-article-1',
     description: 'A page that user sent data from',
   })
+  @IsOptional()
+  @IsString()
   pg?: string
 
   @ApiProperty({
     example: 'en-GB',
     description: "User's locale",
   })
+  @IsOptional()
+  @IsString()
   lc?: string
 
   // Error metrics
@@ -36,27 +53,39 @@ export class ErrorDTO {
     maxLength: 200,
   })
   @IsNotEmpty()
+  @IsString()
+  @MaxLength(200)
   name: string
 
   @ApiProperty({
     example: 'Malformed input',
     maxLength: 2000,
   })
+  @IsOptional()
+  @IsString()
+  @MaxLength(2000)
   message?: string
 
   @ApiProperty({
     example: 12,
   })
+  @IsOptional()
+  @IsNumber()
   lineno?: number
 
   @ApiProperty({
     example: 510,
   })
+  @IsOptional()
+  @IsNumber()
   colno?: number
 
   @ApiProperty({
     example: 'https://example.com/assets/convert.js',
     maxLength: 1000,
   })
+  @IsOptional()
+  @IsString()
+  @MaxLength(1000)
   filename?: string
 }
