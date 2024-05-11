4fcef97679487de6664df07631209c1368cf0390
Blue Mouse <durexp@protonmail.ch>
2024-05-11 13:43:40 +0100

    fix: bug icon in dark theme is dark

--- app/pages/Project/View/components/WaitingForAnError.tsx
+++ app/pages/Project/View/components/WaitingForAnError.tsx
@@ -13,7 +13,10 @@ const WaitingForAnError = () => {
     <div className='px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8'>
       <div className='max-w-max mx-auto'>
         <main className='sm:flex'>
-          <BugAntIcon type='giant' className='h-12 sm:h-20 w-12 sm:w-20 -ml-1.5 mb-2 sm:m-0' />
+          <BugAntIcon
+            type='giant'
+            className='h-12 sm:h-20 w-12 sm:w-20 -ml-1.5 mb-2 sm:m-0 text-gray-900 dark:text-gray-50'
+          />
           <div className='sm:ml-6'>
             <div className='sm:border-l sm:border-gray-200 sm:pl-6'>
               <h1 className='text-4xl font-extrabold text-gray-900 dark:text-gray-50 tracking-tight sm:text-5xl'>
