7c5599532fe38c84aec07d84aef9a4b5b0e09bec
Blue Mouse <durexp@protonmail.ch>
2024-05-19 18:15:59 +0100

    added links to docs / demo

--- app/pages/Contact/index.tsx
+++ app/pages/Contact/index.tsx
@@ -1,8 +1,28 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react'
 import { useTranslation, Trans } from 'react-i18next'
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline'
 
-import { CONTACT_EMAIL, TWITTER_URL, TWITTER_USERNAME, DISCORD_URL } from 'redux/constants'
+import { CONTACT_EMAIL, TWITTER_URL, TWITTER_USERNAME, DISCORD_URL, DOCS_URL, BOOK_A_CALL_URL } from 'redux/constants'
+
+interface IPanel {
+  href: string
+  title: string
+  description: string
+}
+
+const Panel = ({ href, title, description }: IPanel) => (
+  <a
+    href={href}
+    target='_blank'
+    rel='noopener noreferrer'
+    className='relative block rounded-2xl bg-gray-100 hover:bg-gray-200 dark:bg-slate-800/80 dark:hover:bg-slate-800 p-10 border border-gray-300/80 dark:border-slate-900/80'
+  >
+    <ArrowTopRightOnSquareIcon className='w-5 h-5 absolute top-5 right-5 text-gray-900' />
+    <h3 className='text-lg font-semibold leading-7 text-gray-900'>{title}</h3>
+    <p className='mt-3 text-sm leading-6 text-gray-600'>{description}</p>
+  </a>
+)
 
 const Contact = (): JSX.Element => {
   const { t } = useTranslation('common')
@@ -37,6 +57,10 @@ const Contact = (): JSX.Element => {
               ),
             }}
           />
+        </div>
+        <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:col-span-2 lg:gap-8 mt-8'>
+          <Panel href={DOCS_URL} title={t('contact.docs.title')} description={t('contact.docs.desc')} />
+          <Panel href={BOOK_A_CALL_URL} title={t('contact.demo.title')} description={t('contact.demo.desc')} />
         </div>
       </div>
     </div>
