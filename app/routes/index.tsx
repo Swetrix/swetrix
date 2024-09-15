ccbc73744f8d198040ab5e23d695d84069f0cc8b
Blue Mouse <durexp@protonmail.ch>
2024-09-15 21:37:08 +0100

    custom events screenshot

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -693,7 +693,7 @@ const CoreFeatures = ({ theme }: { theme: 'dark' | 'light' }) => {
             aria-hidden='true'
           >
             <div
-              className='mx-auto aspect-[1/3] h-full w-full bg-gradient-to-r from-red-400 to-red-800 opacity-15'
+              className='mx-auto aspect-[1/3] h-full w-full bg-gradient-to-r from-green-400/60 to-green-800/60 opacity-15 dark:from-green-400 dark:to-green-800'
               style={{
                 clipPath:
                   'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
@@ -703,7 +703,7 @@ const CoreFeatures = ({ theme }: { theme: 'dark' | 'light' }) => {
           <div className='h-80 overflow-hidden'>
             <img
               className='object-cover transition-transform group-hover:scale-105'
-              src={theme === 'dark' ? '/assets/performance_part_dark.png' : '/assets/performance_part_light.png'}
+              src={theme === 'dark' ? '/assets/custom_events_dark.png' : '/assets/custom_events_light.png'}
               alt='Website speed and performance monitoring'
             />
           </div>
