1f7b186aacf16f1ef18a00082ac8ed18c28372f8
Blue Mouse <durexp@protonmail.ch>
2024-11-02 16:44:53 +0000

    Update dependencies, remove swetrix.orig.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,38 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
-import typescript from 'rollup-plugin-typescript2'
-import { uglify } from 'rollup-plugin-uglify'
-import pkg from './package.json'
+import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
+import pkg from './package.json' assert { type: 'json' }
 
 export default [
   {
     input: 'src/index.ts',
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
-      { file: pkg.origbrowser, format: 'umd', name: 'swetrix' },
+      { file: pkg.main, format: 'cjs', sourcemap: true },
+      { file: pkg.module, format: 'es', sourcemap: true },
     ],
     plugins: [
-      typescript(), nodeResolve(), commonjs(), sourceMaps(),
-    ]
+      typescript({
+        outDir: './dist',
+        sourceMap: true,
+      }),
+      nodeResolve(),
+      commonjs(),
+      sourceMaps(),
+    ],
   },
   {
     input: 'src/index.ts',
-    output: [
-      { file: pkg.browser, format: 'umd', name: 'swetrix' },
-    ],
+    output: [{ file: pkg.browser, format: 'umd', name: 'swetrix', sourcemap: true }],
     plugins: [
-      typescript(), nodeResolve(), commonjs(), uglify(),
-    ]
-  }
+      typescript({
+        outDir: './dist',
+        sourceMap: true,
+      }),
+      nodeResolve(),
+      commonjs(),
+      terser(),
+    ],
+  },
 ]
