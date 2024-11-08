32cd4f0a1c530208f1ba7184109a9fa28ae6ca24
Blue Mouse <durexp@protonmail.ch>
2024-11-08 19:15:37 +0000

    Expences -> Expenses typo fix

--- web/app/pages/OpenStartup/index.tsx
+++ web/app/pages/OpenStartup/index.tsx
@@ -15,236 +15,236 @@ import { LIVE_DEMO_URL } from 'redux/constants'
 // Revenue taken from Paddle dashboard
 const financeData = {
   '2021-08-01': {
-    'Technical Expences': -32,
-    'Business Expences': 0,
+    'Technical Expenses': -32,
+    'Business Expenses': 0,
     MRR: 0,
     Revenue: 0,
   },
   '2021-09-01': {
-    'Technical Expences': -32,
-    'Business Expences': 0,
+    'Technical Expenses': -32,
+    'Business Expenses': 0,
     MRR: 0,
     Revenue: 0,
   },
   '2021-10-01': {
-    'Technical Expences': -32,
-    'Business Expences': 0,
+    'Technical Expenses': -32,
+    'Business Expenses': 0,
     MRR: 0,
     Revenue: 0,
   },
   '2021-11-01': {
-    'Technical Expences': -32,
-    'Business Expences': 0,
+    'Technical Expenses': -32,
+    'Business Expenses': 0,
     MRR: 0,
     Revenue: 0,
   },
   '2021-12-01': {
-    'Technical Expences': -32,
-    'Business Expences': 0,
+    'Technical Expenses': -32,
+    'Business Expenses': 0,
     MRR: 0,
     Revenue: 0,
   },
   '2022-01-01': {
-    'Technical Expences': -32,
-    'Business Expences': 0,
+    'Technical Expenses': -32,
+    'Business Expenses': 0,
     MRR: 0,
     Revenue: 0,
   },
   '2022-02-01': {
-    'Technical Expences': -32,
-    'Business Expences': 0,
+    'Technical Expenses': -32,
+    'Business Expenses': 0,
     MRR: 2,
     Revenue: 2,
   },
   '2022-03-01': {
-    'Technical Expences': -32,
-    'Business Expences': 0,
+    'Technical Expenses': -32,
+    'Business Expenses': 0,
     MRR: 15,
     Revenue: 14,
   },
   '2022-04-01': {
-    'Technical Expences': -32,
-    'Business Expences': -300,
+    'Technical Expenses': -32,
+    'Business Expenses': -300,
     MRR: 15,
     Revenue: 14,
   },
   '2022-05-01': {
-    'Technical Expences': -32,
-    'Business Expences': -300,
+    'Technical Expenses': -32,
+    'Business Expenses': -300,
     MRR: 15,
     Revenue: 14,
   },
   '2022-06-01': {
-    'Technical Expences': -32,
-    'Business Expences': -300,
+    'Technical Expenses': -32,
+    'Business Expenses': -300,
     MRR: 15,
     Revenue: 14,
   },
   '2022-07-01': {
-    'Technical Expences': -32,
-    'Business Expences': -300,
+    'Technical Expenses': -32,
+    'Business Expenses': -300,
     MRR: 15,
     Revenue: 14,
   },
   '2022-08-01': {
-    'Technical Expences': -42,
-    'Business Expences': -300,
+    'Technical Expenses': -42,
+    'Business Expenses': -300,
     MRR: 15,
     Revenue: 14,
   },
   '2022-09-01': {
-    'Technical Expences': -42,
-    'Business Expences': -350,
+    'Technical Expenses': -42,
+    'Business Expenses': -350,
     MRR: 15,
     Revenue: 14,
   },
   '2022-10-01': {
-    'Technical Expences': -48,
-    'Business Expences': -300,
+    'Technical Expenses': -48,
+    'Business Expenses': -300,
     MRR: 15,
     Revenue: 14,
   },
   '2022-11-01': {
-    'Technical Expences': -48,
-    'Business Expences': -300,
+    'Technical Expenses': -48,
+    'Business Expenses': -300,
     MRR: 15,
     Revenue: 14,
   },
   '2022-12-01': {
-    'Technical Expences': -48,
-    'Business Expences': -400,
+    'Technical Expenses': -48,
+    'Business Expenses': -400,
     MRR: 15,
     Revenue: 14,
   },
   '2023-01-01': {
-    'Technical Expences': -48,
-    'Business Expences': -375,
+    'Technical Expenses': -48,
+    'Business Expenses': -375,
     MRR: 15,
     Revenue: 14,
   },
   '2023-02-01': {
-    'Technical Expences': -48,
-    'Business Expences': -300,
+    'Technical Expenses': -48,
+    'Business Expenses': -300,
     MRR: 35,
     Revenue: 31,
   },
   '2023-03-01': {
-    'Technical Expences': -48,
-    'Business Expences': -370,
+    'Technical Expenses': -48,
+    'Business Expenses': -370,
     MRR: 30,
     Revenue: 26,
   },
   '2023-04-01': {
-    'Technical Expences': -48,
-    'Business Expences': -50,
+    'Technical Expenses': -48,
+    'Business Expenses': -50,
     MRR: 34,
     Revenue: 73,
   },
   '2023-05-01': {
-    'Technical Expences': -48,
-    'Business Expences': -650,
+    'Technical Expenses': -48,
+    'Business Expenses': -650,
     MRR: 38,
     Revenue: 73,
   },
   '2023-06-01': {
-    'Technical Expences': -48,
-    'Business Expences': -300,
+    'Technical Expenses': -48,
+    'Business Expenses': -300,
     MRR: 38,
     Revenue: 22,
   },
   '2023-07-01': {
-    'Technical Expences': -23,
-    'Business Expences': -300,
+    'Technical Expenses': -23,
+    'Business Expenses': -300,
     MRR: 63,
     Revenue: 63,
   },
   '2023-08-01': {
-    'Technical Expences': -23,
-    'Business Expences': 0,
+    'Technical Expenses': -23,
+    'Business Expenses': 0,
     MRR: 64,
     Revenue: 49,
   },
   '2023-09-01': {
-    'Technical Expences': -23,
-    'Business Expences': 0,
+    'Technical Expenses': -23,
+    'Business Expenses': 0,
     MRR: 58,
     Revenue: 86,
   },
   '2023-10-01': {
-    'Technical Expences': -23,
-    'Business Expences': 0,
+    'Technical Expenses': -23,
+    'Business Expenses': 0,
     MRR: 68,
     Revenue: 47,
   },
   '2023-11-01': {
-    'Technical Expences': -23,
-    'Business Expences': 0,
+    'Technical Expenses': -23,
+    'Business Expenses': 0,
     MRR: 77,
     Revenue: 100,
   },
   '2023-12-01': {
-    'Technical Expences': -23,
-    'Business Expences': 0,
+    'Technical Expenses': -23,
+    'Business Expenses': 0,
     MRR: 101,
     Revenue: 216,
   },
   '2024-01-01': {
-    'Technical Expences': -23,
-    'Business Expences': -366,
+    'Technical Expenses': -23,
+    'Business Expenses': -366,
     MRR: 119,
     Revenue: 307,
   },
   '2024-02-01': {
-    'Technical Expences': -23,
-    'Business Expences': 0,
+    'Technical Expenses': -23,
+    'Business Expenses': 0,
     MRR: 129,
     Revenue: 403,
   },
   '2024-03-01': {
-    'Technical Expences': -23,
-    'Business Expences': -252, // google ads campaign
+    'Technical Expenses': -23,
+    'Business Expenses': -252, // google ads campaign
     MRR: 144,
     Revenue: 150,
   },
   '2024-04-01': {
-    'Technical Expences': -23,
-    'Business Expences': -255, // google ads campaign
+    'Technical Expenses': -23,
+    'Business Expenses': -255, // google ads campaign
     MRR: 148,
     Revenue: 1536,
   },
   '2024-05-01': {
-    'Technical Expences': -40,
-    'Business Expences': -45, // [personal expence for business] Wise
+    'Technical Expenses': -40,
+    'Business Expenses': -45, // [personal expence for business] Wise
     MRR: 159,
     Revenue: 1300,
   },
   '2024-06-01': {
-    'Technical Expences': -40,
-    'Business Expences': -252, // marketing
+    'Technical Expenses': -40,
+    'Business Expenses': -252, // marketing
     MRR: 192,
     Revenue: 1624,
   },
   '2024-07-01': {
-    'Technical Expences': -40,
-    'Business Expences': -695, // marketing + marketing freelancer
+    'Technical Expenses': -40,
+    'Business Expenses': -695, // marketing + marketing freelancer
     MRR: 204,
     Revenue: 264,
   },
   '2024-08-01': {
-    'Technical Expences': -45,
-    'Business Expences': -898, // marketing + marketing freelancer
+    'Technical Expenses': -45,
+    'Business Expenses': -898, // marketing + marketing freelancer
     MRR: 204,
     Revenue: 781,
   },
   '2024-09-01': {
-    'Technical Expences': -69,
-    'Business Expences': -1005, // marketing + marketing freelancer
+    'Technical Expenses': -69,
+    'Business Expenses': -1005, // marketing + marketing freelancer
     MRR: 214,
     Revenue: 818,
   },
   '2024-10-01': {
-    'Technical Expences': -75, // cloud hosting, mail
-    'Business Expences': -1148, // google ads + marketing freelancer
+    'Technical Expenses': -75, // cloud hosting, mail
+    'Business Expenses': -1148, // google ads + marketing freelancer
     MRR: 238,
     Revenue: 6378,
   },
@@ -252,14 +252,14 @@ const financeData = {
 
 const financeDataToColumns = (financeData: any) => {
   const dates = Object.keys(financeData)
-  const columns = [['Technical Expences'], ['Business Expences'], ['MRR'], ['Profit'], ['Revenue'], ['x']]
+  const columns = [['Technical Expenses'], ['Business Expenses'], ['MRR'], ['Profit'], ['Revenue'], ['x']]
 
   dates.forEach((date) => {
-    columns[0].push(financeData[date]['Technical Expences'])
-    columns[1].push(financeData[date]['Business Expences'])
+    columns[0].push(financeData[date]['Technical Expenses'])
+    columns[1].push(financeData[date]['Business Expenses'])
     columns[2].push(financeData[date]['MRR'])
     columns[3].push(
-      financeData[date]['Revenue'] + financeData[date]['Technical Expences'] + financeData[date]['Business Expences'],
+      financeData[date]['Revenue'] + financeData[date]['Technical Expenses'] + financeData[date]['Business Expenses'],
     )
     columns[4].push(financeData[date]['Revenue'])
     columns[5].push(date)
@@ -270,7 +270,7 @@ const financeDataToColumns = (financeData: any) => {
 
 const columns = financeDataToColumns(financeData)
 
-const groups = ['Revenue', 'MRR', 'Profit', 'Technical Expences', 'Business Expences']
+const groups = ['Revenue', 'MRR', 'Profit', 'Technical Expenses', 'Business Expenses']
 
 const getSettings = () => {
   return {
@@ -279,20 +279,20 @@ const getSettings = () => {
       xFormat: '%b %Y',
       columns,
       types: {
-        'Technical Expences': bar(),
-        'Business Expences': bar(),
+        'Technical Expenses': bar(),
+        'Business Expenses': bar(),
         MRR: bar(),
         Profit: line(),
         Revenue: line(),
       },
       colors: {
-        'Technical Expences': '#EC4319',
-        'Business Expences': '#F27059',
+        'Technical Expenses': '#EC4319',
+        'Business Expenses': '#F27059',
         MRR: '#709775',
         Profit: '#A5E6AB',
         Revenue: '#12731E',
       },
-      groups: [['MRR', 'Technical Expences', 'Business Expences']],
+      groups: [['MRR', 'Technical Expenses', 'Business Expenses']],
     },
     axis: {
       x: {
