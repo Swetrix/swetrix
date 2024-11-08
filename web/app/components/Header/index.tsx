ff7ee1bd5c9c42915404d7d6db2db8782b5a767e
Blue Mouse <durexp@protonmail.ch>
2024-11-08 17:48:30 +0000

    Remove Changelog from header

--- web/app/components/Header/index.tsx
+++ web/app/components/Header/index.tsx
@@ -388,20 +388,6 @@ const ProfileMenu = ({
                 )}
               </Menu>
 
-              {!isSelfhosted && (
-                <MenuItem>
-                  {({ active }) => (
-                    <Link
-                      to={routes.changelog}
-                      className={cx('block px-4 py-2 text-sm text-gray-700 dark:text-gray-50', {
-                        'bg-gray-100 dark:bg-slate-800': active,
-                      })}
-                    >
-                      {t('footer.changelog')}
-                    </Link>
-                  )}
-                </MenuItem>
-              )}
               {isSelfhosted ? (
                 <MenuItem>
                   {({ active }) => (
