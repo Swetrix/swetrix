e73bfdc0c2e43bc4036b91150654778f8b830534
Blue Mouse <durexp@protonmail.ch>
2025-02-08 03:08:26 +0000

    monospaced font

--- web/app/pages/ErrorTracking/index.tsx
+++ web/app/pages/ErrorTracking/index.tsx
@@ -10,7 +10,6 @@ import _isEmpty from 'lodash/isEmpty'
 
 import routesPath from '~/utils/routes'
 import { getAccessToken } from '~/utils/accessToken'
-import { nFormatterSeparated } from '~/utils/generic'
 import { ERROR_TRACKING_LIVE_DEMO_URL, isBrowser } from '~/lib/constants'
 import { StateType } from '~/lib/store/index'
 import BackgroundSvg from '~/ui/icons/BackgroundSvg'
@@ -18,7 +17,7 @@ import BackgroundSvg from '~/ui/icons/BackgroundSvg'
 import Header from '~/components/Header'
 import Pricing from '../../components/marketing/Pricing'
 import { DitchGoogle } from '~/components/marketing/DitchGoogle'
-import { SquareArrowOutUpRightIcon } from 'lucide-react'
+import { PeopleLoveSwetrix } from '../Performance'
 
 const Lines = () => (
   <div className='pointer-events-none relative'>
@@ -36,14 +35,10 @@ const ErrorTracking = ({ ssrTheme, ssrAuthenticated }: ErrorTrackingProps) => {
   const { t } = useTranslation('common')
   const reduxTheme = useSelector((state: StateType) => state.ui.theme.theme)
   const { authenticated: reduxAuthenticated, loading } = useSelector((state: StateType) => state.auth)
-  const { stats, lastBlogPost } = useSelector((state: StateType) => state.ui.misc)
+  const { lastBlogPost } = useSelector((state: StateType) => state.ui.misc)
   const theme = isBrowser ? reduxTheme : ssrTheme
   const accessToken = getAccessToken()
   const authenticated = isBrowser ? (loading ? !!accessToken : reduxAuthenticated) : ssrAuthenticated
-
-  const events = nFormatterSeparated(Number(stats.events))
-  const users = nFormatterSeparated(Number(stats.users))
-  const websites = nFormatterSeparated(Number(stats.projects))
 
   return (
     <div className='overflow-hidden'>
@@ -80,20 +75,6 @@ const ErrorTracking = ({ ssrTheme, ssrAuthenticated }: ErrorTrackingProps) => {
             />
           </div>
           <Header ssrTheme={ssrTheme} authenticated={authenticated} transparent />
-          <div className='flex items-center justify-center px-2 py-2'>
-            <a
-              href='https://u24.gov.ua/'
-              target='_blank'
-              rel='noreferrer noopener'
-              className='border-b-2 border-transparent text-center text-slate-900 hover:border-slate-900 dark:text-white dark:hover:border-white'
-            >
-              {t('main.ukrSupport')}
-            </a>
-            <SquareArrowOutUpRightIcon
-              className='ml-1 hidden h-4 w-4 text-slate-800 md:block dark:text-white'
-              strokeWidth={1.5}
-            />
-          </div>
           <div className='relative mx-auto min-h-[740px] pt-10 pb-5 sm:px-3 lg:px-6 lg:pt-24 xl:px-8'>
             <div className='relative z-20 flex flex-row content-between justify-center lg:justify-start 2xl:mr-[14vw] 2xl:justify-center'>
               <div className='relative px-4 text-left lg:mt-0 lg:mr-14'>
@@ -108,7 +89,7 @@ const ErrorTracking = ({ ssrTheme, ssrAuthenticated }: ErrorTrackingProps) => {
                     }}
                   />
                 </h1>
-                <div className='mt-2 mb-2 flex items-center overflow-hidden sm:text-xl lg:text-lg xl:text-lg'>
+                <div className='mt-2 mb-2 flex items-center overflow-hidden font-mono sm:text-xl lg:text-lg xl:text-lg'>
                   <p className='rounded-full bg-indigo-500/10 px-3 py-1 text-center text-sm leading-6 font-semibold text-indigo-600 ring-1 ring-indigo-500/20 ring-inset dark:text-indigo-400'>
                     Latest news
                   </p>
@@ -132,10 +113,10 @@ const ErrorTracking = ({ ssrTheme, ssrAuthenticated }: ErrorTrackingProps) => {
                     </ClientOnly>
                   )}
                 </div>
-                <p className='text-base leading-8 text-slate-900 sm:text-xl lg:text-lg xl:text-lg dark:text-slate-300'>
+                <p className='font-mono text-base leading-8 text-slate-900 sm:text-xl lg:text-lg xl:text-lg dark:text-slate-300'>
                   {t('errors.description')}
                 </p>
-                <div className='mt-10 flex flex-col items-center sm:flex-row'>
+                <div className='mt-10 flex flex-col items-center font-mono sm:flex-row'>
                   <Link
                     to={routesPath.signup}
                     className='group flex h-12 w-full items-center justify-center rounded-md bg-slate-900 text-white ring-1 shadow-xs ring-slate-900 transition-all !duration-300 hover:bg-slate-700 sm:mr-6 sm:max-w-[210px] dark:bg-indigo-700 dark:ring-indigo-700 dark:hover:bg-indigo-600'
@@ -199,7 +180,7 @@ const ErrorTracking = ({ ssrTheme, ssrAuthenticated }: ErrorTrackingProps) => {
 
         <div className='mx-auto mt-12 max-w-7xl bg-white px-4 pb-16 whitespace-pre-line dark:bg-slate-900'>
           <h2 className='text-4xl font-extrabold text-slate-900 dark:text-white'>{t('errors.fast.title')}</h2>
-          <p className='mt-6 text-lg text-gray-900 dark:text-gray-50'>
+          <p className='mt-6 font-mono text-lg text-gray-900 dark:text-gray-50'>
             <Trans
               t={t}
               i18nKey='errors.fast.desc'
@@ -226,7 +207,7 @@ const ErrorTracking = ({ ssrTheme, ssrAuthenticated }: ErrorTrackingProps) => {
               }}
             />
           </p>
-          <ul className='mt-2 list-inside list-disc text-lg text-gray-900 dark:text-gray-50'>
+          <ul className='mt-2 list-inside list-disc font-mono text-lg text-gray-900 dark:text-gray-50'>
             {_map(t('errors.fast.list', { returnObjects: true }), (item) => (
               <li key={item} className='mb-2'>
                 {item}
@@ -235,12 +216,12 @@ const ErrorTracking = ({ ssrTheme, ssrAuthenticated }: ErrorTrackingProps) => {
           </ul>
 
           <h2 className='mt-10 text-4xl font-extrabold text-slate-900 dark:text-white'>{t('errors.track.title')}</h2>
-          <p className='mt-6 text-lg text-gray-900 dark:text-gray-50'>{t('errors.track.desc')}</p>
+          <p className='mt-6 font-mono text-lg text-gray-900 dark:text-gray-50'>{t('errors.track.desc')}</p>
 
           <h2 className='mt-10 text-4xl font-extrabold text-slate-900 dark:text-white'>
             {t('performance.privacy.title')}
           </h2>
-          <p className='mt-6 text-lg text-gray-900 dark:text-gray-50'>{t('performance.privacy.desc')}</p>
+          <p className='mt-6 font-mono text-lg text-gray-900 dark:text-gray-50'>{t('performance.privacy.desc')}</p>
         </div>
 
         {/* For now let's hide Pricing for authenticated users on the main page as the Paddle script only loads on the Billing page */}
@@ -262,53 +243,7 @@ const ErrorTracking = ({ ssrTheme, ssrAuthenticated }: ErrorTrackingProps) => {
           <div className='absolute top-52 -left-9 rotate-90'>
             <BackgroundSvg theme={theme} type='shapes' />
           </div>
-          <div className='mx-auto w-full max-w-5xl px-3'>
-            <div className='mx-auto w-full max-w-prose'>
-              <h2 className='text-center text-3xl font-extrabold text-gray-900 md:text-4xl dark:text-white'>
-                {t('main.peopleLoveSwetrix')}
-              </h2>
-              <p className='mx-auto mt-5 max-w-prose text-center text-xl text-gray-600 dark:text-gray-200'>
-                {t('main.whyPeopleLoveSwetrix')}
-              </p>
-            </div>
-            <div className='mt-20 flex flex-col items-center justify-between md:mt-32 md:flex-row'>
-              <div className='text-center'>
-                <ClientOnly fallback={<p className='text-center text-5xl font-extrabold text-indigo-700'>0</p>}>
-                  {() => (
-                    <p className='text-center text-5xl font-extrabold text-indigo-700'>
-                      {users[0]}
-                      {users[1] && <span className='text-gray-900 dark:text-indigo-200'>{users[1]}+</span>}
-                    </p>
-                  )}
-                </ClientOnly>
-                <p className='text-lg text-gray-600 dark:text-gray-200'>{t('main.users')}</p>
-              </div>
-              <div className='mx-5 mt-16 mb-14 h-2 w-2 rounded-full bg-gray-800 md:mt-0 md:mb-0 dark:bg-gray-200' />
-              <div className='text-center'>
-                <ClientOnly fallback={<p className='text-center text-5xl font-extrabold text-indigo-700'>0</p>}>
-                  {() => (
-                    <p className='text-center text-5xl font-extrabold text-indigo-700'>
-                      {websites[0]}
-                      {websites[1] && <span className='text-gray-900 dark:text-indigo-200'>{websites[1]}+</span>}
-                    </p>
-                  )}
-                </ClientOnly>
-                <p className='text-lg text-gray-600 dark:text-gray-200'>{t('main.websites')}</p>
-              </div>
-              <div className='mx-5 mt-16 mb-14 h-2 w-2 rounded-full bg-gray-800 md:mt-0 md:mb-0 dark:bg-gray-200' />
-              <div className='text-center'>
-                <ClientOnly fallback={<p className='text-center text-5xl font-extrabold text-indigo-700'>0</p>}>
-                  {() => (
-                    <p className='text-center text-5xl font-extrabold text-indigo-700'>
-                      {events[0]}
-                      {events[1] && <span className='text-gray-900 dark:text-indigo-200'>{events[1]}+</span>}
-                    </p>
-                  )}
-                </ClientOnly>
-                <p className='text-lg text-gray-600 dark:text-gray-200'>{t('main.pageviews')}</p>
-              </div>
-            </div>
-          </div>
+          <PeopleLoveSwetrix />
         </section>
         {/* end Become a developer */}
       </main>
