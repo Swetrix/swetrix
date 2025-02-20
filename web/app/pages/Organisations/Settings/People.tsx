4e657f95f871a08c8eec3fe91b7235379df01fdf
Blue Mouse <durexp@protonmail.ch>
2025-02-20 21:59:06 +0000

    Fix overflow issue

--- web/app/pages/Organisations/Settings/People.tsx
+++ web/app/pages/Organisations/Settings/People.tsx
@@ -290,10 +290,10 @@ const People = ({ organisation, reloadOrganisation }: PeopleProps) => {
         {_isEmpty(members) ? (
           <NoPeople />
         ) : (
-          <div className='mt-3 flex flex-col'>
-            <div className='-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+          <div className='mt-3 flex flex-col font-mono'>
+            <div className='-mx-4 -my-2 overflow-x-auto sm:-mx-6 md:overflow-x-visible lg:-mx-8'>
               <div className='inline-block min-w-full py-2 align-middle md:px-6 lg:px-8'>
-                <div className='overflow-hidden ring-1 ring-black/10 md:rounded-lg'>
+                <div className='ring-1 ring-black/10 md:rounded-lg'>
                   <table className='min-w-full divide-y divide-gray-300 dark:divide-gray-600'>
                     <thead>
                       <tr className='dark:bg-slate-800'>
