bf74ce8c91ed1e7aa9cb3de497fef215fadc64bf
Blue Mouse <durexp@protonmail.ch>
2025-04-14 18:39:11 +0100

    Remove ProjectForShared

--- web/app/lib/models/SharedProject.ts
+++ web/app/lib/models/SharedProject.ts
@@ -1,15 +1,11 @@
 import { Project } from './Project'
 
-export interface ProjectForShared extends Project {
-  shared?: boolean
-}
-
 export interface SharedProject {
   id: string
   confirmed: boolean
   role: string
   created: string
   updated: string
-  project: ProjectForShared
+  project: Project
   uiHidden: boolean
 }
