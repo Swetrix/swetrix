ed4dbabf0e461184f963543074c8947e3a81f713
Blue Mouse <durexp@protonmail.ch>
2025-04-16 22:42:04 +0100

    fix migration

--- backend/migrations/clickhouse/2025_04_16.js
+++ backend/migrations/clickhouse/2025_04_16.js
@@ -19,14 +19,12 @@ const queries = [
   `ALTER TABLE ${dbName}.analytics UPDATE os = 'macOS' WHERE os = 'Mac OS'`,
   `ALTER TABLE ${dbName}.customEV UPDATE os = 'macOS' WHERE os = 'Mac OS'`,
   `ALTER TABLE ${dbName}.errors UPDATE os = 'macOS' WHERE os = 'Mac OS'`,
-  `ALTER TABLE ${dbName}.performance UPDATE os = 'macOS' WHERE os = 'Mac OS'`,
   `ALTER TABLE ${dbName}.captcha UPDATE os = 'macOS' WHERE os = 'Mac OS'`,
 
   // Change Chromium OS to Chrome OS
   `ALTER TABLE ${dbName}.analytics UPDATE os = 'Chrome OS' WHERE os = 'Chromium OS'`,
   `ALTER TABLE ${dbName}.customEV UPDATE os = 'Chrome OS' WHERE os = 'Chromium OS'`,
   `ALTER TABLE ${dbName}.errors UPDATE os = 'Chrome OS' WHERE os = 'Chromium OS'`,
-  `ALTER TABLE ${dbName}.performance UPDATE os = 'Chrome OS' WHERE os = 'Chromium OS'`,
   `ALTER TABLE ${dbName}.captcha UPDATE os = 'Chrome OS' WHERE os = 'Chromium OS'`,
 ]
 
