ddd13a86706398b6346983fda4437828304cca57
Blue Mouse <durexp@protonmail.ch>
2025-09-21 18:07:13 +0100

    fix: CE recovery

--- web/app/routes/password-reset.$id.tsx
+++ web/app/routes/password-reset.$id.tsx
@@ -1,8 +1,6 @@
 import type { HeadersFunction } from 'react-router'
-import { redirect } from 'react-router'
 import type { SitemapFunction } from 'remix-sitemap'
 
-import { isSelfhosted } from '~/lib/constants'
 import CreateNewPassword from '~/pages/Auth/CreateNewPassword'
 
 export const headers: HeadersFunction = ({ parentHeaders }) => {
@@ -13,14 +11,6 @@ export const headers: HeadersFunction = ({ parentHeaders }) => {
 export const sitemap: SitemapFunction = () => ({
   exclude: true,
 })
-
-export async function loader() {
-  if (isSelfhosted) {
-    return redirect('/login', 302)
-  }
-
-  return null
-}
 
 export default function Index() {
   return <CreateNewPassword />
--- web/app/routes/recovery.tsx
+++ web/app/routes/recovery.tsx
@@ -1,20 +1,15 @@
 import type { HeadersFunction } from 'react-router'
-import { redirect } from 'react-router'
+import { SitemapFunction } from 'remix-sitemap'
 
-import { isSelfhosted } from '~/lib/constants'
 import ForgotPassword from '~/pages/Auth/ForgotPassword'
+
+export const sitemap: SitemapFunction = () => ({
+  exclude: true,
+})
 
 export const headers: HeadersFunction = ({ parentHeaders }) => {
   parentHeaders.set('X-Frame-Options', 'DENY')
   return parentHeaders
-}
-
-export async function loader() {
-  if (isSelfhosted) {
-    return redirect('/login', 302)
-  }
-
-  return null
 }
 
 export default function Index() {
