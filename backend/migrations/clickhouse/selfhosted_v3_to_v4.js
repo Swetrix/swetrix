0928d41a0a637d8bfe67140a82e8270e3a377a2f
Blue Mouse <durexp@protonmail.ch>
2025-09-23 00:34:12 +0100

    add missing migrations

--- backend/migrations/clickhouse/selfhosted_v3_to_v4.js
+++ backend/migrations/clickhouse/selfhosted_v3_to_v4.js
@@ -1,4 +1,31 @@
+const childProcess = require('child_process')
+const path = require('path')
 const { queriesRunner, dbName } = require('./setup')
+
+function runScript(scriptPath) {
+  return new Promise((resolve, reject) => {
+    const fullPath = path.join(__dirname, scriptPath)
+    console.log('[INFO] Running script: ' + fullPath)
+
+    const process = childProcess.fork(fullPath)
+    let invoked = false
+
+    // listen for errors as they may prevent the exit event from firing
+    process.on('error', function (err) {
+      if (invoked) return
+      invoked = true
+      reject(err)
+    })
+
+    // execute the callback once the process has finished running
+    process.on('exit', function (code) {
+      if (invoked) return
+      invoked = true
+      const err = code === 0 ? null : new Error('exit code ' + code)
+      err ? reject(err) : resolve()
+    })
+  })
+}
 
 const queries = [
   `CREATE TABLE IF NOT EXISTS ${dbName}.user
@@ -37,6 +64,12 @@ const queries = [
 async function runMigrations() {
   try {
     await queriesRunner(queries)
+    await runScript('./2025_01_09.js')
+    await runScript('./2025_02_08.js')
+    await runScript('./2025_04_16.js')
+    await runScript('./2025_05_17.js')
+    await runScript('./2025_05_23.js')
+
     console.log('[INFO] SWETRIX CE V4 - MIGRATIONS FINISHED')
   } catch (reason) {
     console.error('Error running migration:', reason)
