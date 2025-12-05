c66f1f2add43608cfdde46272230f80489e8b076
Blue Mouse <durexp@protonmail.ch>
2025-12-05 01:32:54 +0000

    salt entity - primary storage is mysql

--- backend/apps/cloud/src/analytics/analytics.module.ts
+++ backend/apps/cloud/src/analytics/analytics.module.ts
@@ -1,14 +1,21 @@
 import { Module, forwardRef } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { AnalyticsService } from './analytics.service'
 import { AnalyticsController } from './analytics.controller'
 import { SaltService } from './salt.service'
+import { Salt } from './entities/salt.entity'
 import { UserModule } from '../user/user.module'
 import { AppLoggerModule } from '../logger/logger.module'
 import { ProjectModule } from '../project/project.module'
 
 @Module({
-  imports: [forwardRef(() => UserModule), AppLoggerModule, ProjectModule],
+  imports: [
+    TypeOrmModule.forFeature([Salt]),
+    forwardRef(() => UserModule),
+    AppLoggerModule,
+    ProjectModule,
+  ],
   providers: [AnalyticsService, SaltService],
   exports: [AnalyticsService, SaltService],
   controllers: [AnalyticsController],
