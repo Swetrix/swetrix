a397d117a55c38b721addcc743926b213048b293
Blue Mouse <durexp@protonmail.ch>
2025-12-22 01:58:11 +0000

    Remove extensions SDK docs

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -70,7 +70,6 @@ module.exports = {
         ],
       },
     ],
-    'Extensions SDK': ['ext-sdk-introduction', 'ext-sdk-faq', 'ext-sdk-reference'],
     API: ['api/stats', 'api/events', 'api/admin'],
     CAPTCHA: ['captcha/introduction', 'captcha/client-side-usage', 'captcha/server-side-validation', 'captcha/testing'],
     'Self-hosting': ['selfhosting/how-to', 'selfhosting/configuring'],
