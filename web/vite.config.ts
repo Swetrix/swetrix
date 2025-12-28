f3dd8aa3a1a51431da619c9cd6717dff4a44e210
Blue Mouse <durexp@protonmail.ch>
2025-12-28 02:31:06 +0000

    Add PORT variable

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   server: {
-    port: 3000,
+    port: Number(process.env.PORT) || 3000,
   },
   optimizeDeps: {
     include: ['axios-auth-refresh'],
