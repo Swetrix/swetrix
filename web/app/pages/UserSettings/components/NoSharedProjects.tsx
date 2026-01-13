e99bf237771d890f35c1237274236d05719cb8db
Blue Mouse <durexp@protonmail.ch>
2026-01-13 20:14:12 +0000

    Nicer empty state UI

--- web/app/pages/UserSettings/components/NoSharedProjects.tsx
+++ web/app/pages/UserSettings/components/NoSharedProjects.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next'
+import { ShareIcon } from '@heroicons/react/24/outline'
 
 import { Text } from '~/ui/Text'
 
@@ -6,12 +7,13 @@ const NoSharedProjects = () => {
   const { t } = useTranslation('common')
 
   return (
-    <div className='flex flex-col py-6 sm:px-6 lg:px-8'>
-      <div className='mx-auto w-full max-w-7xl'>
-        <Text as='h2' size='2xl' className='mb-4 text-center leading-snug'>
-          {t('profileSettings.noSharedProjects')}
-        </Text>
+    <div className='flex flex-col items-center justify-center py-8 text-center'>
+      <div className='mb-3 flex size-10 items-center justify-center rounded-lg bg-gray-100 dark:bg-slate-800'>
+        <ShareIcon className='size-5 text-gray-400 dark:text-slate-500' />
       </div>
+      <Text as='p' size='sm' colour='secondary'>
+        {t('profileSettings.noSharedProjects')}
+      </Text>
     </div>
   )
 }
