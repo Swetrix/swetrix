8acc5cc6de4a456949eb8eceff8c6c3507a5a14c
Blue Mouse <durexp@protonmail.ch>
2026-01-20 01:10:49 +0000

    fix startup

--- backend/apps/cloud/src/twoFactorAuth/twoFactorAuth.module.ts
+++ backend/apps/cloud/src/twoFactorAuth/twoFactorAuth.module.ts
@@ -8,7 +8,7 @@ import { MailerModule } from '../mailer/mailer.module'
 
 @Module({
   imports: [
-    UserModule,
+    forwardRef(() => UserModule),
     AppLoggerModule,
     forwardRef(() => AuthModule),
     MailerModule,
