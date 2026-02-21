da18eb59045e16404e2afc08b93cb793e9cbe446
Blue Mouse <durexp@protonmail.ch>
2026-02-21 02:11:25 +0000

    Set base path to /

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,7 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  basePath: '/docs',
+  basePath: '/',
 };
 
 export default withMDX(config);
