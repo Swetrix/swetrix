cd59b3fec1bd443d05223e8e141e4ec8c5374cac
Blue Mouse <durexp@protonmail.ch>
2026-02-21 20:25:30 +0000

    fix: docs startup errors

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,9 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  turbopack: {
+    root: import.meta.dirname,
+  },
 };
 
 export default withMDX(config);
