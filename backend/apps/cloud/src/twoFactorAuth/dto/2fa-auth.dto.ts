c364a5787108d6da275440bb248de1d92ab1bd82
Blue Mouse <durexp@protonmail.ch>
2026-03-05 01:05:43 +0000

    Validate twoFactorAuthenticationCode

--- backend/apps/cloud/src/twoFactorAuth/dto/2fa-auth.dto.ts
+++ backend/apps/cloud/src/twoFactorAuth/dto/2fa-auth.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty } from 'class-validator'
+import { IsNotEmpty, IsString, Matches } from 'class-validator'
 
 export class TwoFactorAuthDTO {
   @ApiProperty({ example: '123456', required: true })
   @IsNotEmpty()
+  @IsString()
+  @Matches(/^\d{6}$/)
   twoFactorAuthenticationCode: string
 }
