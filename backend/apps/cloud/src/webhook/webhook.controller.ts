d172c6d8bcd74993a372a2c6c2ecbe1c433c065e
Blue Mouse <durexp@protonmail.ch>
2026-03-26 19:28:23 +0000

    fix: parse userId from subscription_payment_succeeded passthrough as a fallback to subId check

--- backend/apps/cloud/src/webhook/webhook.controller.ts
+++ backend/apps/cloud/src/webhook/webhook.controller.ts
@@ -232,11 +232,30 @@ export class WebhookController {
       }
 
       case 'subscription_payment_succeeded': {
-        const { subscription_id: subID, next_bill_date: nextBillDate } = body
+        const {
+          passthrough,
+          subscription_id: subID,
+          next_bill_date: nextBillDate,
+        } = body
 
-        const subscriber = await this.userService.findOne({
+        let uid
+        try {
+          uid = JSON.parse(passthrough)?.uid
+        } catch {
+          this.logger.error(
+            `[subscription_payment_succeeded] Cannot parse the uid: ${JSON.stringify(body)}`,
+          )
+        }
+
+        let subscriber = await this.userService.findOne({
           where: { subID },
         })
+
+        if (!subscriber && uid) {
+          subscriber = await this.userService.findOne({
+            where: { id: uid },
+          })
+        }
 
         if (!subscriber) {
           this.logger.error(
@@ -267,8 +286,8 @@ export class WebhookController {
         }
 
         if (Object.keys(updateParams).length > 0) {
-          await this.userService.updateBySubID(subID, updateParams)
-          await this.projectService.clearProjectsRedisCacheBySubId(subID)
+          await this.userService.update(subscriber.id, updateParams)
+          await this.projectService.clearProjectsRedisCache(subscriber.id)
         }
 
         break
