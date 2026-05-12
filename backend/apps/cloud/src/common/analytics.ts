85776027a6b1a01139f4e9c0874edf2914169f89
Blue Mouse <durexp@protonmail.ch>
2026-05-13 00:15:16 +0100

    Don't require swetrix tracking project id

--- backend/apps/cloud/src/common/analytics.ts
+++ backend/apps/cloud/src/common/analytics.ts
@@ -1,7 +1,7 @@
 import { Swetrix, type TrackEventOptions } from '@swetrix/node'
 import { isDevelopment } from './constants'
 
-const swetrix = new Swetrix(process.env.SWETRIX_PID, {
+const swetrix = new Swetrix(process.env.SWETRIX_PID || 'NO_PROJECT_ID', {
   // Same port as in main.ts
   apiURL: 'http://localhost:5005/log',
   devMode: isDevelopment,
@@ -12,6 +12,10 @@ export const trackCustom = async (
   userAgent: string,
   options: TrackEventOptions,
 ) => {
+  if (!process.env.SWETRIX_PID) {
+    return
+  }
+
   try {
     await swetrix.track(ip, userAgent, options)
   } catch (reason) {
